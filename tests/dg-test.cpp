76056da52453c8d7542682e4dc47c48426be89b9
Marek Chalupa <mchqwerty@gmail.com>
2015-03-07 07:03:32 -0500

    fix dg-test
    Wrong return values and indentation. Do it until there's not much of
    it...

--- tests/dg-test.cpp
+++ tests/dg-test.cpp
@@ -5,76 +5,81 @@
 
 using namespace dg;
 
-static bool check(int expr, const char *fmt, ...)
+/* return true when expr is violated and false when
+ * it is OK */
+static bool check(int expr, const char *func, const char *fmt, ...)
 {
-	va_list args;
+    va_list args;
 
-	if (expr)
-		return false;
+    if (expr)
+        return false;
 
-	va_start(args, fmt);
-	vfprintf(stderr, fmt, args);
-	va_end(args);
+	fprintf(stderr, "%s -", func);
 
-	fputc('\n', stderr);
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
 
-	return true;
+    fputc('\n', stderr);
+
+    return true;
 }
 
 #define chck_init() bool __chck_ret = false;
 // return false when some check failed
-#define chck_ret() return (!__chck_ret);
+#define chck_ret() return __chck_ret;
 #define chck_dump(d)\
-	do { if (__chck_ret) (d)->dump(); } while(0)
-#define chck(expr, ...)	\
-	do { __chck_ret |= check((expr), __VA_ARGS__); } while(0)
+    do { if (__chck_ret) (d)->dump(); } while(0)
+#define chck(expr, ...)    \
+    do { __chck_ret |= check((expr), __func__, __VA_ARGS__); } while(0)
 
 static bool add_test1(void)
 {
-	chck_init();
+    chck_init();
 
-	DependenceGraph d;
-	DGNode n1, n2;
+    DependenceGraph d;
+    DGNode n1, n2;
 
-	chck(n1.addControlEdge(&n2), "adding C edge claims it is there");
-	chck(n2.addDependenceEdge(&n1), "adding D edge claims it is there");
+    chck(n1.addControlEdge(&n2), "adding C edge claims it is there");
+    chck(n2.addDependenceEdge(&n1), "adding D edge claims it is there");
 
-	d.addNode(&n1);
-	d.addNode(&n2);
+    d.addNode(&n1);
+    d.addNode(&n2);
 
-	int n = 0;
-	for (auto I = d.begin(), E = d.end(); I != E; ++I) {
-		++n;
-		chck(*I == &n1 || *I == &n2, "Got some garbage in nodes");
-	}
+    int n = 0;
+    for (auto I = d.begin(), E = d.end(); I != E; ++I) {
+        ++n;
+        chck(*I == &n1 || *I == &n2, "Got some garbage in nodes");
+    }
 
-	chck(n == 2, "BUG: adding nodes to graph, got %d instead of 2", n);
+    chck(n == 2, "BUG: adding nodes to graph, got %d instead of 2", n);
 
-	int nn = 0;
-	for (auto NI = n1.control_begin(), NE = n1.control_end(); NI != NE; ++NI){
-		chck(*NI == &n2, "Got wrong control edge");
-		++nn;
-	}
+    int nn = 0;
+    for (auto NI = n1.control_begin(), NE = n1.control_end(); NI != NE; ++NI){
+        chck(*NI == &n2, "Got wrong control edge");
+        ++nn;
+    }
 
-	chck(nn == 1, "BUG: adding control edges, has %d instead of 1", nn);
+    chck(nn == 1, "BUG: adding control edges, has %d instead of 1", nn);
 
-	nn = 0;
-	for (auto NI = n2.dependence_begin(), NE = n2.dependence_end(); NI != NE; ++NI) {
-		chck(*NI == &n1, "Got wrong control edge");
-		++nn;
-	}
+    nn = 0;
+    for (auto NI = n2.dependence_begin(), NE = n2.dependence_end();
+         NI != NE; ++NI) {
+        chck(*NI == &n1, "Got wrong control edge");
+        ++nn;
+    }
 
-	chck(nn == 1, "BUG: adding dep edges, has %d instead of 1", nn);
+    chck(nn == 2, "BUG: adding dep edges, has %d instead of 1", nn);
 
-	chck_dump(&d);
-	chck_ret();
+    chck_dump(&d);
+    chck_ret();
 }
 
 int main(int argc, char *argv[])
 {
-	bool ret = true;
+    bool ret = false;
 
-	ret |= add_test1();
+    ret |= add_test1();
 
-	return ret;
+    return ret;
 }
