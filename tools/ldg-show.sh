f71701d63a78ffcb7d0ecce2fc5af5410a1eb85f
Marek Chalupa <mchqwerty@gmail.com>
2015-03-26 15:53:23 -0400

    tools: add ldg-show.sh script
    This is just simple wrapper around llvm-dg-dump, so that I don't
    have to do everything manually

---
+++ tools/ldg-show.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DIR=`dirname $0`
+
+#defult program
+GUI=xdg-open
+
+if which okular &>/dev/null; then
+	GUI=okular
+elif which evince &>/dev/null; then
+	GUI=evince
+fi
+
+$DIR/llvm-dg-dump "$1" > ldg-show-output.dot && dot -Tps ldg-show-output.dot > ldg-show-output.ps && $GUI ldg-show-output.ps
+rm ldg-show-output*
