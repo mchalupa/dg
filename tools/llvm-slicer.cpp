107e5573e79833cfb774167d5d6a87392d0fd7fb
Marek Chalupa <mchqwerty@gmail.com>
2015-09-08 18:33:52 +0200

    add llvm-slicer
    This is basically the same as llvm-dg-dump, just only slices
    the code + it will have some more fetures that are not
    dependent on the graph 8-)
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tools/llvm-slicer.cpp
@@ -0,0 +1,119 @@
+#include <assert.h>
+#include <cstdio>
+
+#include <set>
+
+#ifndef HAVE_LLVM
+#error "This code needs LLVM enabled"
+#endif
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/raw_os_ostream.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+
+#include <iostream>
+#include <fstream>
+#include "llvm/LLVMDependenceGraph.h"
+#include "llvm/PointsTo.h"
+#include "llvm/DefUse.h"
+#include "llvm/Slicer.h"
+#include "Utils.h"
+
+using namespace dg;
+using llvm::errs;
+
+int main(int argc, char *argv[])
+{
+    llvm::LLVMContext context;
+    llvm::SMDiagnostic SMD;
+    std::unique_ptr<llvm::Module> M;
+
+    const char *slicing_criterion = NULL;
+    const char *module = NULL;
+
+    // parse options
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-c") == 0
+            || strcmp(argv[i], "-crit") == 0
+            || strcmp(argv[i], "-slice") == 0){
+            slicing_criterion = argv[++i];
+        } else {
+            module = argv[i];
+        }
+    }
+
+    if (!slicing_criterion || !module) {
+        errs() << "Usage: % [-c|-crit|-slice] func_call module\n";
+        return 1;
+    }
+
+    M = llvm::parseIRFile(module, SMD, context);
+    if (!M) {
+        SMD.print(argv[0], errs());
+        return 1;
+    }
+
+    debug::TimeMeasure tm;
+    LLVMDependenceGraph d;
+
+    std::set<LLVMNode *> gatheredCallsites;
+    d.gatherCallsites(slicing_criterion, &gatheredCallsites);
+    // build the graph
+    d.build(&*M);
+
+    if (gatheredCallsites.empty()) {
+        if (strcmp(slicing_criterion, "ret") == 0)
+            gatheredCallsites.insert(d.getExit());
+        else {
+            errs() << "Did not find slicing criterion: " << slicing_criterion << "\n";
+            return 1;
+        }
+    }
+
+    analysis::LLVMPointsToAnalysis PTA(&d);
+
+    tm.start();
+    PTA.run();
+    tm.stop();
+    tm.report("INFO: Points-to analysis took");
+
+    analysis::LLVMDefUseAnalysis DUA(&d);
+
+    tm.start();
+    DUA.run();  // compute reaching definitions
+    tm.stop();
+    tm.report("INFO: Reaching defs analysis took");
+
+    tm.start();
+    DUA.addDefUseEdges(); // add def-use edges according that
+    tm.stop();
+    tm.report("INFO: Adding Def-Use edges took");
+
+    LLVMSlicer slicer;
+    uint32_t slid = 0;
+
+    tm.start();
+    for (LLVMNode *start : gatheredCallsites)
+        slid = slicer.mark(start, slid);
+
+    slicer.slice(&d, nullptr, slid);
+
+    tm.stop();
+    tm.report("INFO: Slicing took");
+
+    auto st = slicer.getStatistics();
+    errs() << "INFO: Sliced away " << st.second
+           << " from " << st.first << " nodes\n";
+
+    std::string fl(module);
+    fl.replace(fl.end() - 3, fl.end(), ".sliced");
+    std::ofstream ofs(fl);
+    llvm::raw_os_ostream output(ofs);
+    llvm::WriteBitcodeToFile(&*M, output);
+
+    return 0;
+}
