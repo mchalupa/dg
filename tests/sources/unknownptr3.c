7b0d05377dcfb19884ba79e727fb5650d6cf0f0a
Marek Chalupa <mchqwerty@gmail.com>
2015-09-15 12:01:07 +0200

    tests: add another test
    slicing with pointer with unknown offset
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/unknownptr3.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+
+volatile int getidx(void)
+{
+	return 2;
+}
+
+int main(void)
+{
+	int array[5];
+	int idx = getidx();
+
+	// some pointer arithmetic
+	int *p = array;
+	p += idx;
+	++p;
+
+	*p = 7;
+
+	assert(array[3] == 7);
+	return 0;
+}
