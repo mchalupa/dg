b1806888f891b9ad85a49959c58b53079f01f394
Marek Chalupa <mchqwerty@gmail.com>
2015-09-15 11:54:05 +0200

    tests: add another test for pointer with unknown offset
    typical accessing array with variable index
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/unknownptr2.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+
+volatile int getidx(void)
+{
+	return 2;
+}
+
+int main(void)
+{
+	int array[5];
+	int idx = getidx();
+	// getidx() returns 2, but slicer
+	// does not know it, so array[idx] is
+	// pointer with unknown offset to array
+	array[idx] = 7;
+
+	assert(array[2] == 7);
+	return 0;
+}
