611e4fd3ab3362377623ab412d37d4c5b54e7254
Marek Chalupa <mchqwerty@gmail.com>
2015-09-16 12:09:49 +0200

    analysis-generic: add DataLayout.h header
    we use the type there
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- src/llvm/AnalysisGeneric.cpp
+++ src/llvm/AnalysisGeneric.cpp
@@ -3,6 +3,7 @@
 #include <llvm/IR/Value.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Constants.h>
+#include <llvm/IR/DataLayout.h>
 #include <llvm/Support/raw_ostream.h>
 
 #include "AnalysisGeneric.h"
