f40375326ac56673bf8302d327994b44383649b9
Marek Chalupa <mchqwerty@gmail.com>
2015-09-20 15:28:20 +0200

    tests: add another test for llvm.memcpy
    no with function pointers, check for issue #18
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/llvmmemcpy2.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+
+typedef struct Stuff {
+ void (*f1)(int *);
+ void (*f2)(int *);
+} Stuff;
+
+void foo1(int *a)
+{
+	*a = 1;
+}
+
+void foo2(int *a)
+{
+	*a = 2;
+}
+
+int main()
+{
+ int a = 0;
+ Stuff good = {foo1, foo2};
+ Stuff bad;
+ bad = good;
+ bad.f2(&a);
+ assert(a == 2);
+ return 0;
+}
