a085ce9edb3b7f123c9739ff9ab1f101a53decbb
Marek Chalupa <mchqwerty@gmail.com>
2015-09-26 23:14:06 +0200

    control-deps: fix infinite loops
    Infinite loops are problem, because they have no post-dominator
    tree and therefore no control dependence is built for them. That
    is unsound. Add unprecise but sound edges if we cannot add
    normal control dependencies. At least until we implement a better
    algorithm.
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- src/llvm/LLVMDependenceGraph.cpp
+++ src/llvm/LLVMDependenceGraph.cpp
@@ -512,6 +512,7 @@ void LLVMDependenceGraph::computePostDominators(bool addPostDomFrontiers)
 
         // add immediate post-dominator edges
         auto our_blocks = F.second->getConstructedBlocks();
+        bool built = false;
         for (auto it : our_blocks) {
             LLVMBBlock *BB = it.second;
             BasicBlock *B = const_cast<BasicBlock *>(it.first);
@@ -523,6 +524,7 @@ void LLVMDependenceGraph::computePostDominators(bool addPostDomFrontiers)
 
             DomTreeNode *idom = N->getIDom();
             BasicBlock *idomBB = idom ? idom->getBlock() : nullptr;
+            built = true;
 
             if (idomBB) {
                 LLVMBBlock *pb = our_blocks[idomBB];
@@ -542,6 +544,17 @@ void LLVMDependenceGraph::computePostDominators(bool addPostDomFrontiers)
                 }
 
                 BB->setIPostDom(root);
+            }
+        }
+
+        // well, if we haven't built the pdtree, this is probably infinite loop
+        // that has no pdtree. Until we have anything better, just add sound
+        // edges that are not so precise - to predecessors.
+        if (!built) {
+            for (auto it : our_blocks) {
+                LLVMBBlock *BB = it.second;
+                for (LLVMBBlock *succBB : BB->successors())
+                    succBB->addPostDomFrontier(BB);
             }
         }
 
