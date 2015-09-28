59b795f2f3bfe358c79e22367e26285438c07bdc
Marek Chalupa <mchqwerty@gmail.com>
2015-09-28 16:33:31 +0200

    tests: add more tests with linked lists
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/list5.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct wl_list {
+	struct wl_list *prev;
+	struct wl_list *next;
+};
+
+struct item {
+	int number;
+	struct wl_list link;
+};
+
+int main(void)
+{
+	struct wl_list list;
+	struct item *i1 = malloc(sizeof *i1);
+
+	// inlined wl_list_init
+	list.prev = &list;
+	list.next = &list;
+
+	// inlined wl_list_insert
+	i1->link.prev = &list;
+	i1->link.next = list.next;
+	list.next = &i1->link;
+	i1->link.next->prev = &i1->link;
+
+	// inlined part of wl_list_remove
+	i1->link.prev->next = i1->link.next;
+	i1->link.next->prev = i1->link.prev;
+
+	assert(list.next == list.prev);
+	return 0;
+}
