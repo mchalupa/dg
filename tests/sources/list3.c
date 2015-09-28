d728ca62d01aff5ec301ef69578081d7b2ced4bb
Marek Chalupa <mchqwerty@gmail.com>
2015-09-28 13:53:41 +0200

    tests: add few tests with linked list
    the list is taken from wayland
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/list3.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct wl_list;
+
+void wl_list_init(struct wl_list *list);
+void wl_list_insert(struct wl_list *list, struct wl_list *elm);
+void wl_list_remove(struct wl_list *elm);
+int wl_list_length(const struct wl_list *list);
+int wl_list_empty(const struct wl_list *list);
+
+struct wl_list {
+	struct wl_list *prev;
+	struct wl_list *next;
+};
+
+struct item {
+	int number;
+	struct wl_list link;
+};
+
+#define wl_container_of(ptr, sample, member)				\
+	(__typeof__(sample))((char *)(ptr) -				\
+			     offsetof(__typeof__(*sample), member))
+int main(void)
+{
+	struct wl_list list;
+	wl_list_init(&list);
+
+	struct item *i1 = malloc(sizeof *i1);
+	wl_list_insert(&list, &i1->link);
+	i1->number = 8;
+
+	struct item *i = wl_container_of(list.next, i, link);
+	assert(i->number == 8);
+
+	return 0;
+}
---
+++ tests/sources/list4.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct wl_list;
+
+void wl_list_init(struct wl_list *list);
+void wl_list_insert(struct wl_list *list, struct wl_list *elm);
+void wl_list_remove(struct wl_list *elm);
+int wl_list_length(const struct wl_list *list);
+int wl_list_empty(const struct wl_list *list);
+
+struct wl_list {
+	struct wl_list *prev;
+	struct wl_list *next;
+};
+
+struct item {
+	int number;
+	struct wl_list link;
+};
+
+int main(void)
+{
+	struct wl_list list;
+	wl_list_init(&list);
+
+	struct item *i1 = malloc(sizeof *i1);
+	wl_list_insert(&list, &i1->link);
+	wl_list_remove(&i1->link);
+
+	assert(wl_list_empty(&list));
+	return 0;
+}
