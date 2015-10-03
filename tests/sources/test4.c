8db63007c4bc2678b13b8a3cf6c11cbc80e7d2fe
Marek Chalupa <mchqwerty@gmail.com>
2015-10-03 23:03:03 +0200

    tests: add few more tests
    slicing global and generic
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/test4.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+
+int set(int *a, int b)
+{
+	*a = b;
+	return b;
+}
+
+void clear(int *a)
+{
+	set(a, 0);
+}
+
+/* basic test */
+int main(void)
+{
+	int a, b, c;
+	a = 0;
+	b = 1;
+	c = 3;
+
+	a = b + c;
+	b = 3;
+	c = 5;
+
+	clear(&a);
+	set(&a, 11);
+	clear(&a);
+	set(&a, 12);
+	clear(&a);
+	set(&a, 13);
+
+	assert(a == 13);
+	return 0;
+}
