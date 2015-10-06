09874666d0c82e39481e8b17bceddcf1704f8564
Marek Chalupa <mchqwerty@gmail.com>
2015-10-06 16:22:48 +0200

    tests: add two more tests for funcptrs
    return allocated memory from function that allocates
    the memory via func ptr call
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/funcptr10.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+
+struct item {
+	int a;
+};
+
+struct item *alloc(void)
+{
+	struct item *i = malloc(sizeof *i);
+	i->a = 8;
+	return i;
+}
+
+struct item *alloc2(void)
+{
+	struct item *i = malloc(sizeof *i);
+	i->a = 9;
+	return i;
+}
+
+struct item *foo(struct item *(*f)(void))
+{
+	return f();
+}
+
+/* test passing function pointers */
+int main(void)
+{
+	int a = 1;
+	struct item *i;
+	if (a > 0)
+		i = foo(alloc2);
+	else
+		i = foo(alloc);
+
+	assert(i->a == 9);
+	return 0;
+}
