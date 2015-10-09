107130b12526a56735afdeeca40a7709c6fc28e3
Marek Chalupa <mchqwerty@gmail.com>
2015-10-09 20:53:15 +0200

    tests: add few tests for slicing complex structures

---
+++ tests/sources/pointers3.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+
+struct item {
+	int a;
+	int *ptr;
+};
+
+struct item2 {
+	struct item i;
+	struct item p;
+};
+
+int main(void)
+{
+	int n = 10;
+	struct item2 *A = malloc(n * sizeof(struct item2));
+	A[3].p.a = 4;
+	A[5].i.ptr = &A[3].p.a;
+	/* make the pointer point to "unknown" location
+	 * for points-to analysis */
+	int idx = 3;
+	int *p = &A[3].p.a;
+
+	assert(*p == 4);
+	return 0;
+}
