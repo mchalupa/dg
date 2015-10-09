912f475f0342e26ef9a6033337a8e73c7ba0a0ec
Marek Chalupa <mchqwerty@gmail.com>
2015-10-09 20:39:23 +0200

    tests: add two tests for slicing switch instruction
    no big deal, just wanted to try that
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/switch1.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+
+int main(void)
+{
+	int a = 1;
+	switch(a) {
+		case 1:
+			a = 4;
+			break;
+		case 2:
+			a = 2;
+			break;
+		case 3:
+			a = 3;
+			break;
+		case 4:
+		case 5:
+			a = 8;
+			break;
+		default:
+			a = 3;
+	}
+
+
+	assert(a == 4);
+	return 0;
+}
