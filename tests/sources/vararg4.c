a0c20c0d96cdfbe216754fb6a197c29170444f91
Marek Chalupa <mchqwerty@gmail.com>
2015-10-13 08:43:07 +0200

    tests: add two more vararg tests
    passing a function as va argument
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/vararg4.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdarg.h>
+
+void setv(int num, ...)
+{
+	va_list l;
+	void (*f)(int *);
+	int *x;
+
+	(void) num;
+
+	va_start(l, num);
+	f = va_arg(l, void *);
+	x = va_arg(l, int *);
+	f(x);
+	va_end(l);
+}
+
+void foo(int *a)
+{
+	*a = 17;
+}
+
+int main(void)
+{
+	int a = 0;
+	setv(1, foo, &a);
+	assert(a == 17);
+	return 0;
+}
