cd53185398eb3ba8d33a6c38f144602d9fb37663
Marek Chalupa <mchqwerty@gmail.com>
2015-10-20 11:32:49 +0200

    tests: add new test
    this test tests allocating memory through function pointer.
    Allocation is ok, but the initialization is sliced away
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/dynalloc5.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct item {
+	int a;
+};
+
+/* use function pointer */
+void *(*memalloc)(unsigned long size) = malloc;
+struct item *alloc()
+{
+	struct item *i = memalloc(sizeof *i);
+	i->a = 13;
+
+	return i;
+}
+
+int main(void)
+{
+	struct item *i = alloc();
+	assert(i->a == 13);
+	return 0;
+}
