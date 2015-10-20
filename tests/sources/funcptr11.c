d89a4ad61a6da1755536975778d6e0596c0bed3f
Marek Chalupa <mchqwerty@gmail.com>
2015-10-20 11:13:13 +0200

    tests: add two more function pointers tests
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/funcptr11.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+
+int *foo(int *a, int *b)
+{
+	return a;
+}
+
+int *(*f)(int *, int *) = 0;
+
+int main(void)
+{
+	int a, b;
+	int *p;
+
+	f = foo;
+	p = f(&a, &b);
+	*p = 7;
+
+	assert(a == 7);
+	return 0;
+}
