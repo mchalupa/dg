ac46ea94658f65fc5299b69bbc9117b9d6642c2a
Marek Chalupa <mchqwerty@gmail.com>
2015-10-26 09:20:43 +0100

    tests: add new tests
    these two tests reveal a bug in interprocedural slicing
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/sum.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+
+#define N 4
+int sum(int x[N])
+{
+  int i;
+  long long ret;
+  ret = 0;
+  for (i = 0; i < N; i++) {
+    ret = ret + x[i];
+  }
+  return ret;
+}
+
+int main(void)
+{
+  int x[N] = {1, 2, 3, 4};
+  int temp, i, ret, ret2;
+
+  /* sum elements in array */
+  ret = sum(x);
+
+  /* do cyclic shift in array */
+  temp=x[0];
+  for(i = 0 ; i < N - 1; i++){
+     x[i] = x[i + 1];
+  }
+  x[N - 1] = temp;
+
+  /* sum them again */
+  ret2 = sum(x);
+
+  /* check that the sums equal*/
+  assert(ret == ret2);
+  return 0;
+}
