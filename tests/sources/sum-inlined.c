dee7991322b025c9d9652237158cf0d1559c7ce0
Marek Chalupa <mchqwerty@gmail.com>
2015-10-26 09:20:43 +0100

    tests: add new tests
    these two tests reveal a bug in interprocedural slicing
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/sum-inlined.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+
+#define N 4
+
+int main(void)
+{
+  int x[N] = {1, 2, 3, 4};
+  int temp, i, ret, ret2;
+
+  /* sum elements in array */
+  ret = 0;
+  for (i = 0; i < N; i++) {
+    ret = ret + x[i];
+  }
+
+  /* do cyclic shift in array */
+  temp=x[0];
+  for(i = 0 ; i < N - 1; i++){
+     x[i] = x[i + 1];
+  }
+  x[N - 1] = temp;
+
+  /* sum them again */
+  ret2 = 0;
+  for (i = 0; i < N; i++) {
+    ret2 = ret2 + x[i];
+  }
+
+  /* check that the sums equal*/
+  assert(ret == ret2);
+  return 0;
+}
---
+++ tests/sources/sum2.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+
+int sum(int x[3])
+{
+  long long ret;
+  ret = x[0] + x[1] + x[2];
+  return ret;
+}
+
+/* use concrete offsets, so that
+ * we won't have any UNKNOWN_OFFSET */
+int main(void)
+{
+  int x[3] = {1, 2, 3};
+  int temp, i, ret, ret2;
+
+  /* sum elements in array */
+  ret = sum(x);
+
+  /* do cyclic shift in array */
+  temp=x[0];
+  x[0] = x[1];
+  x[1] = x[2];
+  x[2] = temp;
+
+  /* sum them again */
+  ret2 = sum(x);
+
+  /* check that the sums equal*/
+  assert(ret == ret2);
+  return 0;
+}
