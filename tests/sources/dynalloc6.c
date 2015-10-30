cd4ff8795c6f629796e102fcf4cbbbe526a7290a
Marek Chalupa <mchqwerty@gmail.com>
2015-10-30 22:24:27 +0100

    tests: add test for dynamic allocation
    this is a test for a regression
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/dynalloc6.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+
+struct node {
+    int val;
+    struct node *next;
+    struct node *prev;
+};
+static struct node* alloc_node(void)
+{
+    struct node *ptr = malloc(sizeof *ptr);
+    ptr->val = 13;
+    ptr->next = ((void *)0);
+    ptr->prev = ((void *)0);
+    return ptr;
+}
+static void chain_node(struct node **ppnode)
+{
+    struct node *node = alloc_node();
+    node->next = *ppnode;
+    *ppnode = node;
+}
+static void create_sll(const struct node **pp1)
+{
+    chain_node(pp1);
+}
+
+int main()
+{
+    const struct node *p1;
+    create_sll(&p1);
+    assert(p1->val == 13);
+
+    return 0;
+}
