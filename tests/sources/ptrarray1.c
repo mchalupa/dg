0a5f24d8ada9f7ed789437df84691bf7707165a5
Marek Chalupa <mchqwerty@gmail.com>
2015-12-22 09:42:16 +0100

    tests: add two new tests
    Just got thought for a nice test, so I wrote it down.
    It is not to show any bug
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tests/sources/ptrarray1.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+
+int main(void)
+{
+	int array[10];
+	int *ptrs[10];
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		ptrs[i - 1] = &array[i];
+
+	ptrs[0] = &array[9];
+	*ptrs[0] = 1;
+	array[0] = 2;
+
+	assert(array[9] == 1);
+	return 0;
+}
