1d528a156fb0b01757a2756186cb65757022a547
Marek Chalupa <mchqwerty@gmail.com>
2016-01-31 19:42:21 +0100

    tests: make the rest of tests using test_assert
    there were few tests left that were using normal assert()
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- tests/slicing-phi2.sh
+++ tests/slicing-phi2.sh
@@ -6,6 +6,10 @@ set -e
 TESTS_DIR=`dirname $0`
 source "$TESTS_DIR/test-runner.sh"
 
+run_test "sources/phi1.c"
+
+echo -e "\nwithout PHI nodes it's OK\n"
+
 set_environment
 
 CODE="$TESTS_DIR/sources/phi2.c"
@@ -13,21 +17,13 @@ NAME=${CODE%.*}
 BCFILE="$NAME.bc"
 SLICEDFILE="$NAME.sliced"
 
-clang -emit-llvm -c "$CODE" -o "$BCFILE"
-llvm-slicer -c __assert_fail "$BCFILE"
-
-# run the sliced code and check if it
-# has everything to pass the assert in it
-lli "$SLICEDFILE"
-
-echo -e "\nwithout PHI nodes it's OK\n"
-
 # now run the test with pure SSA form where
 # are phi nodes
 PHIBC="$NAME-phi.bc"
 opt -mem2reg "$BCFILE" -o "$PHIBC"
 
 SLICEDFILE="$NAME-phi.sliced"
-llvm-slicer -c __assert_fail "$PHIBC"
+llvm-slicer -c test_assert "$PHIBC"
+link_with_assert "$PHIBC" "$PHIBC.linked"
 
-lli "$SLICEDFILE"
+get_result "$PHIBC.linked"
