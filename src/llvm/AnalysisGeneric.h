427234e83f83c19e5937907dcc3fda89617a8c73
Marek Chalupa <mchqwerty@gmail.com>
2016-03-10 16:41:31 +0100

    factor out Offset class into new file
    we had the code duplicated...
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- src/llvm/AnalysisGeneric.h
+++ src/llvm/AnalysisGeneric.h
@@ -5,6 +5,8 @@
 #include <set>
 #include <assert.h>
 
+#include "analysis/Offset.h"
+
 namespace llvm {
     class Value;
     class ConstantExpr;
@@ -17,53 +19,6 @@ class LLVMDependenceGraph;
 class LLVMNode;
 
 namespace analysis {
-
-#define UNKNOWN_OFFSET ~((uint64_t) 0)
-
-// just a wrapper around uint64_t to
-// handle UNKNOWN_OFFSET somehow easily
-// maybe later we'll make it a range
-struct Offset
-{
-    Offset(uint64_t o = UNKNOWN_OFFSET) : offset(o) {}
-    Offset& operator+=(const Offset& o)
-    {
-        if (offset == UNKNOWN_OFFSET)
-            return *this;
-
-        if (o.offset == UNKNOWN_OFFSET)
-            offset = UNKNOWN_OFFSET;
-        else
-            offset += o.offset;
-
-        return *this;
-    }
-
-    Offset operator+(const Offset& o)
-    {
-        if (offset == UNKNOWN_OFFSET || o.offset == UNKNOWN_OFFSET)
-            return UNKNOWN_OFFSET;
-
-        return Offset(offset + o.offset);
-    }
-
-    bool operator<(const Offset& o) const
-    {
-        return offset < o.offset;
-    }
-
-    bool inRange(uint64_t from, uint64_t to) const
-    {
-        return (offset >= from && offset <= to);
-    }
-
-    bool isUnknown() const { return offset == UNKNOWN_OFFSET; }
-
-    uint64_t operator*() const { return offset; }
-    const uint64_t *operator->() const { return &offset; }
-
-    uint64_t offset;
-};
 
 struct MemoryObj;
 struct Pointer
