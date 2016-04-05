77c2f4c987257f1aeacc92983dae036c5838f44f
Marek Chalupa <mchqwerty@gmail.com>
2016-04-05 17:50:09 +0200

    add llvm-pss-compare tool
    This tool computes flow-sensitive & flow-insensitive
    points-to sets and checks if flow-sensitive info
    is subset of flow-insensitive. Also can run
    just one analysis without anything else - that
    can be used for profiling
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ tools/llvm-pss-compare.cpp
@@ -0,0 +1,226 @@
+#include <assert.h>
+#include <cstdio>
+
+#include <set>
+
+#ifndef HAVE_LLVM
+#error "This code needs LLVM enabled"
+#endif
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/raw_os_ostream.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "llvm/LLVMPointsToAnalysis.h"
+#include "analysis/PointsToFlowInsensitive.h"
+#include "analysis/PointsToFlowSensitive.h"
+
+#include "analysis/Pointer.h"
+
+#include "Utils.h"
+
+using namespace dg;
+using namespace dg::analysis::pss;
+using llvm::errs;
+
+enum PTType {
+    FLOW_SENSITIVE = 1,
+    FLOW_INSENSITIVE,
+};
+
+static std::string
+getInstName(const llvm::Value *val)
+{
+    std::ostringstream ostr;
+    llvm::raw_os_ostream ro(ostr);
+
+    assert(val);
+    ro << *val;
+    ro.flush();
+
+    // break the string if it is too long
+    return ostr.str();
+}
+
+static void
+printName(PSSNode *node)
+{
+    const char *name = node->getName();
+    std::string nm;
+    if (!name) {
+        if (!node->getUserData<llvm::Value>()) {
+            printf("%p\n", node);
+
+            return;
+        }
+
+        nm = getInstName(node->getUserData<llvm::Value>());
+        name = nm.c_str();
+    }
+
+    // escape the " character
+    for (int i = 0; name[i] != '\0'; ++i) {
+        // crop long names
+        if (i >= 70) {
+            printf(" ...");
+            break;
+        }
+
+        if (name[i] == '"')
+            putchar('\\');
+
+        putchar(name[i]);
+    }
+}
+
+static void
+dumpPSSNode(PSSNode *n)
+{
+    const char *name = n->getName();
+
+    printf("NODE: ");
+    printName(n);
+
+    if (n->getSize() || n->isHeap() || n->isZeroInitialized())
+        printf(" [size: %lu, heap: %u, zeroed: %u]",
+               n->getSize(), n->isHeap(), n->isZeroInitialized());
+
+    if (n->pointsTo.empty()) {
+        puts(" -- no points-to");
+        return;
+    } else
+        putchar('\n');
+
+    for (const Pointer& ptr : n->pointsTo) {
+        printf("    -> ");
+        printName(ptr.target);
+        if (ptr.offset.isUnknown())
+            puts(" + UNKNOWN_OFFSET");
+        else
+            printf(" + %lu\n", *ptr.offset);
+    }
+}
+
+static bool verify_ptsets(LLVMPointsToAnalysis *fi, LLVMPointsToAnalysis *fs)
+{
+    bool ok = true;
+    const std::unordered_map<const llvm::Value *, PSSNode *>& mp = fi->getNodesMap();
+    for (auto it : mp) {
+        PSSNode *fsnode = fs->getPointsTo(it.first);
+        if (!fsnode) {
+            llvm::errs() << "FS don't have points-to for: " << *it.first << "\n"
+                         << "but FI has:\n";
+            dumpPSSNode(it.second);
+            ok = false;
+            continue;
+        }
+
+        if (!it.second) {
+            llvm::errs() << "PSS node is null: " << *it.first << "\n";
+            ok = false;
+            continue;
+        }
+
+        for (const Pointer& ptr : it.second->pointsTo) {
+            bool found = false;
+            for (const Pointer& ptr2 : fsnode->pointsTo) {
+                // FIXME: what about UNKNOWN_OFFSETS?
+                if ((ptr2.target->getUserData<llvm::Value>()
+                    == ptr.target->getUserData<llvm::Value>())
+                    && (ptr2.offset == ptr.offset)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                    llvm::errs() << "FS not subset of FI: " << *it.first << "\n";
+                    llvm::errs() << "FI:\n";
+                    dumpPSSNode(it.second);
+                    llvm::errs() << "FS:\n";
+                    dumpPSSNode(fsnode);
+                    llvm::errs() << " ---- \n";
+                    ok = false;
+            }
+        }
+    }
+
+    return ok;
+}
+
+int main(int argc, char *argv[])
+{
+    llvm::LLVMContext context;
+    llvm::SMDiagnostic SMD;
+    llvm::Module *M;
+    const char *module = nullptr;
+    unsigned type = FLOW_SENSITIVE | FLOW_INSENSITIVE;
+
+    // parse options
+    for (int i = 1; i < argc; ++i) {
+        // run only given points-to analysis
+        if (strcmp(argv[i], "-pta") == 0) {
+            if (strcmp(argv[i+1], "fs") == 0)
+                type = FLOW_SENSITIVE;
+            else if (strcmp(argv[i+1], "fi") == 0)
+                type = FLOW_INSENSITIVE;
+            else {
+                errs() << "Unknown PTA type" << argv[i + 1] << "\n";
+                abort();
+            }
+        /*} else if (strcmp(argv[i], "-v") == 0) {
+            verbose = true;*/
+        } else {
+            module = argv[i];
+        }
+    }
+
+    if (!module) {
+        errs() << "Usage: % llvm-pss-compare [-pta fs|fi] IR_module\n";
+        return 1;
+    }
+
+    M = llvm::ParseIRFile(module, SMD, context);
+    if (!M) {
+        SMD.print(argv[0], errs());
+        return 1;
+    }
+
+    debug::TimeMeasure tm;
+
+    LLVMPointsToAnalysis *PTAfs;
+    LLVMPointsToAnalysis *PTAfi;
+    if (type & FLOW_INSENSITIVE) {
+        PTAfi = new LLVMPointsToAnalysisImpl<analysis::pss::PointsToFlowInsensitive>(M);
+
+        tm.start();
+        PTAfi->run();
+        tm.stop();
+        tm.report("INFO: Points-to flow-insensitive analysis took");
+    }
+
+    if (type & FLOW_SENSITIVE) {
+            PTAfs = new LLVMPointsToAnalysisImpl<analysis::pss::PointsToFlowSensitive>(M);
+        tm.start();
+        PTAfs->run();
+        tm.stop();
+        tm.report("INFO: Points-to flow-sensitive analysis took");
+    }
+
+    int ret = 0;
+    if (type == (FLOW_SENSITIVE | FLOW_INSENSITIVE)) {
+        ret = !verify_ptsets(PTAfi, PTAfs);
+        if (ret == 0)
+            llvm::errs() << "FS is a subset of FI, all OK\n";
+    }
+
+    return ret;
+}
