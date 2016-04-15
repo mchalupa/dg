ab8668db2b758b3eb2a22fef0aeb8d3ba04852ff
Marek Chalupa <mchqwerty@gmail.com>
2016-04-15 11:21:20 +0200

    tests: pointers4 - fix off by one error
    we used A[4] instead of A[3].. Interesting that it
    was ok with llvm 3.4
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- tests/sources/pointers4.c
+++ tests/sources/pointers4.c
@@ -14,12 +14,14 @@ int main(void)
 {
 	int n = 10;
 	struct item2 *A = malloc(n * sizeof(struct item2));
+	/* store to variable in A[3] */
 	struct item *i = &A[3].i;
 	i->ptr = &i->a;
 	*i->ptr = 8;
 
+	/* shift to A[3] by incrementing in loop */
 	struct item2 *ptr = A;
-	for (n = 0; n < 4; ++n)
+	for (n = 0; n < 3; ++n)
 		++ptr;
 
 	test_assert(ptr->i.a == 8);
