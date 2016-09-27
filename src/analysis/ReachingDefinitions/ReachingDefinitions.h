23669faff6da7dd9caa7aebf15b03485f8712874
Marek Chalupa <mchqwerty@gmail.com>
2016-09-27 20:18:09 +0200

    RD analysis: change queing of nodes
    use the same queing as in PTA - keep a set/list
    nodes that changed the information and then just
    queue only them and all reachable nodes from them
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- src/analysis/ReachingDefinitions/ReachingDefinitions.h
+++ src/analysis/ReachingDefinitions/ReachingDefinitions.h
@@ -191,6 +191,52 @@ public:
         }
     }
 
+    // get nodes in BFS order and store them into
+    // the container
+    std::vector<RDNode *> getNodes(RDNode *start_node = nullptr,
+                                   std::vector<RDNode *> *start_set = nullptr,
+                                   unsigned expected_num = 0)
+    {
+        assert(root && "Do not have root");
+        assert(!(start_set && start_node)
+               && "Need either starting set or starting node, not both");
+
+        ++dfsnum;
+        ADT::QueueFIFO<RDNode *> fifo;
+
+        if (start_set) {
+            for (RDNode *s : *start_set) {
+                fifo.push(s);
+                s->dfsid = dfsnum;
+            }
+        } else {
+            if (!start_node)
+                start_node = root;
+
+            fifo.push(start_node);
+            start_node->dfsid = dfsnum;
+        }
+
+        std::vector<RDNode *> cont;
+        if (expected_num != 0)
+            cont.reserve(expected_num);
+
+        while (!fifo.empty()) {
+            RDNode *cur = fifo.pop();
+            cont.push_back(cur);
+
+            for (RDNode *succ : cur->successors) {
+                if (succ->dfsid != dfsnum) {
+                    succ->dfsid = dfsnum;
+                    fifo.push(succ);
+                }
+            }
+        }
+
+        return cont;
+    }
+
+
     RDNode *getRoot() const { return root; }
     void setRoot(RDNode *r) { root = r; }
 
@@ -200,18 +246,30 @@ public:
     {
         assert(root && "Do not have root");
 
-        std::set<RDNode *> nodes;
-        getNodes(nodes);
+        std::vector<RDNode *> to_process = std::move(getNodes(root));
+        std::vector<RDNode *> changed;
 
         // do fixpoint
-        bool changed;
         do {
-            changed = false;
+            unsigned last_processed_num = to_process.size();
+            changed.clear();
 
-            for (RDNode *cur : nodes)
-                changed |= processNode(cur);
+            for (RDNode *cur : to_process) {
+                if (processNode(cur))
+                    changed.push_back(cur);
+            }
 
-        } while (changed);
+            if (!changed.empty()) {
+                to_process.clear();
+                to_process = std::move(getNodes(nullptr /* starting node */,
+                                                &changed /* starting set */,
+                                                last_processed_num /* expected num */));
+
+                // since changed was not empty,
+                // the to_process must not be empty too
+                assert(!to_process.empty());
+            }
+        } while (!changed.empty());
     }
 };
 
