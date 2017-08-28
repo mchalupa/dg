bcebde1ea76387939c7033b70961dd45ca1a4e66
Tomas Jasek <tomsik68@gmail.com>
2017-08-28 08:43:10 +0200

    rd: add ssa builder
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

--- src/llvm/analysis/SingleAssignment/SsaBuilder.h
+++ src/llvm/analysis/SingleAssignment/SsaBuilder.h
@@ -1,23 +1,74 @@
-#ifndef SSABUILDER_H
-#define SSABUILDER_H
+#ifndef _DG_SSABUILDER_H_
+#define _DG_SSABUILDER_H_
+
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <stack>
 
 #include "BBlock.h"
 #include "analysis/ReachingDefinitions/ReachingDefinitions.h"
 #include "analysis/DominanceFrontiers.h"
 #include "llvm/analysis/Dominators.h"
+#include "analysis/ReachingDefinitions/Ssa/PhiPlacement.h"
 
 namespace dg {
 namespace analysis {
+namespace rd {
 namespace ssa {
+
 /**
  * Transforms given program to its Static Single Assignment form
  */
 class SsaBuilder
 {
+private:
+
     using NodeT = dg::analysis::rd::RDNode;
     using BlockT = BBlock<NodeT>;
-    using MapT = std::unordered_map<const llvm::Function *, std::map<const llvm::BasicBlock *, BlockT *>>;
+    using CFMapT = std::unordered_map<const llvm::Function *, std::map<const llvm::BasicBlock *, BlockT *>>;
 
+    template <typename _Tp> using StackT = std::stack<_Tp, std::vector<_Tp>>;
+
+    PhiAdditions phi;
+    std::unordered_map<NodeT *, size_t> counters;
+    std::unordered_map<NodeT *, StackT<size_t>> stacks;
+
+    void constructSsa(BlockT *root_block)
+    {
+        counters.clear();
+        stacks.clear();
+
+        search(root_block);
+    }
+
+    void search(BlockT *X)
+    {
+        std::unordered_map<NodeT *, NodeT *> oldLHS;
+        for (NodeT *A : X->getNodes())
+        {
+            if (A->getType() == STORE) {
+                // A is an ordinary assignment...
+                // TODO: for each variable used in RHS???
+            }
+        }
+
+        for (auto& edge : X->successors())
+        {
+            BlockT *Y = edge.target;
+        }
+
+        for (BlockT *Y : X->getDominators())
+        {
+            search(Y);
+        }
+
+        for (NodeT *A : X->getNodes())
+        {
+            if (A->getType() != STORE)
+                continue;
+        }
+    }
 public:
 
     /**
@@ -25,17 +76,25 @@ public:
      * this will create a new graph with root given as unique_ptr
      * the caller is responsible for deallocation of the resulting graph
      */
-    BlockT *build(const BlockT *root, MapT& constructed_functions)
+    NodeT *build(NodeT *root, CFMapT& constructed_functions)
     {
         // calculate dominators, true=calculate also DomFrontiers
         Dominators<NodeT,true> d;
         d.calculate(constructed_functions);
 
+        DugBuilder db;
+
+        PhiPlacement pp;
+        phi = pp.calculate(db.build(root));
+
         return nullptr;
     }
+
+    const PhiAdditions& getPhi() const { return phi; }
 };
 
 }
 }
 }
-#endif /* SSABUILDER_H */
+}
+#endif /* _DG_SSABUILDER_H_ */
