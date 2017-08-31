c98921381e515ff8a304d44d075821887d1503c7
Tomas Jasek <tomsik68@gmail.com>
2017-08-31 15:58:36 +0200

    llvmrd: add SparseRDGraphBuilder
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

---
+++ src/analysis/ReachingDefinitions/Ssa/SparseRDGraphBuilder.h
@@ -0,0 +1,151 @@
+#ifndef _DG_SSABUILDER_H_
+#define _DG_SSABUILDER_H_
+
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+
+#include "BBlock.h"
+#include "analysis/ReachingDefinitions/ReachingDefinitions.h"
+#include "analysis/ReachingDefinitions/Ssa/PhiPlacement.h"
+
+namespace dg {
+namespace analysis {
+namespace rd {
+namespace ssa {
+
+/**
+ * Calculates a Sparse Graph for RD information propagation
+ */
+class SparseRDGraphBuilder
+{
+private:
+    using NodeT = dg::analysis::rd::RDNode;
+    using BlockT = BBlock<NodeT>;
+
+    // NodeT is sometimes used as a CFG node, otherwise as a variable(as a target of DefSite).
+    // this alias should aid readability a little
+    using VarT = NodeT*;
+
+    // just for convenience
+    template <typename _Tp> using StackT = std::stack<_Tp, std::vector<_Tp>>;
+
+public:
+    using SRGEdge = std::pair<NodeT *, NodeT*>;
+    // neighbour lists representation of Sparse Graph
+    //                                       Definition                  Variable    Def/Use
+    using SparseRDGraph = std::unordered_map<NodeT *, std::vector<SRGEdge>>;
+
+private:
+    PhiAdditions phi;
+    SparseRDGraph srg;
+
+    // stack of last definitions for each variable
+    std::unordered_map<VarT, StackT<NodeT *>> stacks;
+
+    // for each assignment contains variables modified by setting its LHS
+    std::unordered_map<NodeT *, std::vector<VarT>> oldLHS;
+
+    std::vector<std::unique_ptr<NodeT>> phi_nodes;
+
+    void constructSrg(BlockT *root_block)
+    {
+        // restart state
+        stacks.clear();
+
+        search(root_block);
+    }
+
+    void addAssignment(NodeT *assignment, VarT var)
+    {
+        oldLHS[assignment].push_back(var);
+
+        if (!stacks[var].empty()) {
+            srg[stacks[var].top()].push_back(std::make_pair(var, assignment));
+        }
+
+        stacks[var].push(assignment);
+    }
+
+    void addUse(NodeT *use, VarT var)
+    {
+        assert( !stacks[var].empty() );
+        srg[stacks[var].top()].push_back(std::make_pair(var, use));
+    }
+
+    void search(BlockT *X)
+    {
+
+        // find assignments in block
+        for (NodeT *A : X->getNodes())
+        {
+            for (const DefSite& cds : A->defs)
+            {
+                VarT var = const_cast<VarT>(cds.target);
+                addAssignment(A, var);
+            }
+            for (const DefSite& use : A->uses)
+            {
+                VarT var = const_cast<VarT>(use.target);
+                addUse(A, var);
+            }
+        }
+
+        // add individual phi-functions as uses
+        for (auto& edge : X->successors())
+        {
+            BlockT *Y = edge.target;
+            for (NodeT *F : Y->getNodes())
+            {
+                for (const DefSite& use : F->getUses()) {
+                    addUse(F, use.target);
+                }
+            }
+        }
+
+        for (BlockT *Y : X->getDominators())
+        {
+            search(Y);
+        }
+
+        for (NodeT *A : X->getNodes())
+        {
+            for (VarT var : oldLHS[A])
+            {
+                stacks[var].pop();
+            }
+        }
+    }
+
+public:
+
+    /**
+     * Builds a sparse graph
+     */
+    SparseRDGraph&& build(NodeT *root)
+    {
+        assert( root && "need root" );
+
+        // find assignments and use them to find places for phi-functions
+        AssignmentFinder af;
+        PhiPlacement pp;
+        phi = pp.calculate(af.build(root));
+
+        // place the phi functions into program
+        phi_nodes = pp.place(phi);
+
+        // now recursively construct the SparseRDGraph
+        constructSrg(root->getBBlock());
+
+        return std::move(srg);
+    }
+};
+
+using SparseRDGraph = SparseRDGraphBuilder::SparseRDGraph;
+
+}
+}
+}
+}
+#endif /* _DG_SSABUILDER_H_ */
