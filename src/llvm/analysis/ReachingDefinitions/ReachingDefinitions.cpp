4fc4326bcc49a9c9c487c10dd47f11c29a0960db
Tomas Jasek <tomsik68@gmail.com>
2017-08-31 15:56:46 +0200

    llvmrd: add LOAD node
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

--- src/llvm/analysis/ReachingDefinitions/ReachingDefinitions.cpp
+++ src/llvm/analysis/ReachingDefinitions/ReachingDefinitions.cpp
@@ -376,6 +376,20 @@ std::vector<DefSite> LLVMRDBuilder::getPointsTo(const llvm::Value *val, RDBlock 
     return result;
 }
 
+RDNode *LLVMRDBuilder::createLoad(const llvm::Instruction *Inst, RDBlock *rb)
+{
+    RDNode *node = new RDNode(RDNodeType::LOAD);
+    addNode(Inst, node);
+    rb->append(node);
+
+    for (unsigned i = 0; i < Inst->getNumOperands(); ++i) {
+        if (Inst->getOperand(i)->getType()->isPointerTy()) {
+            node->addUses(getPointsTo(Inst->getOperand(i), rb));
+        }
+    }
+
+    return node;
+}
 
 RDNode *LLVMRDBuilder::createStore(const llvm::Instruction *Inst, RDBlock *rb)
 {
@@ -485,6 +499,9 @@ LLVMRDBuilder::buildBlock(const llvm::BasicBlock& block)
                     // FIXME: add new type of node NOOP,
                     // and optimize it away later
                     node = createReturn(&Inst, rb);
+                    break;
+                case Instruction::Load:
+                    node = createLoad(&Inst, rb);
                     break;
                 case Instruction::Call:
                     if (!isRelevantCall(&Inst))
