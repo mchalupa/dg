0a1611589c534d7ccfcd64c6777ad067f17bb4da
Marek Chalupa <xchalup4@fi.muni.cz>
2018-01-29 18:35:25 +0100

    inv-PTA: inherit from FS PTA
    Do not duplicate code. Most of the stuff can be inherited
    from FS PTA here (it is a form of FS PTA afterall).
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- src/analysis/PointsTo/PointsToWithInvalidate.h
+++ src/analysis/PointsTo/PointsToWithInvalidate.h
@@ -3,83 +3,33 @@
 
 #include <cassert>
 
-#include "Pointer.h"
-#include "PointerSubgraph.h"
+#include "PointsToFlowSensitive.h"
 
 namespace dg {
 namespace analysis {
 namespace pta {
 
-class PointsToWithInvalidate : public PointerAnalysis
+class PointsToWithInvalidate : public PointsToFlowSensitive
 {
+    static bool canChangeMM(PSNode *n) {
+        if (n->getType() == PSNodeType::FREE ||
+            n->getType() == PSNodeType::INVALIDATE_LOCALS)
+            return true;
+
+        return PointsToFlowSensitive::canChangeMM(n);
+    }
+
+    static bool needsMerge(PSNode *n) {
+        return n->predecessorsNum() > 1 || canChangeMM(n);
+    }
+
 public:
-    using MemoryObjectsSetT = std::set<MemoryObject *>;
-    using MemoryMapT = std::map<const Pointer, MemoryObjectsSetT>;
+    using MemoryMapT = PointsToFlowSensitive::MemoryMapT;
 
     // this is an easy but not very efficient implementation,
     // works for testing
     PointsToWithInvalidate(PointerSubgraph *ps)
-    : PointerAnalysis(ps, UNKNOWN_OFFSET, false, true) {}
-
-    static bool canChangeMM(PSNode *n) {
-        if (n->predecessorsNum() == 0 || // root node
-            n->getType() == PSNodeType::STORE ||
-            n->getType() == PSNodeType::MEMCPY ||
-            n->getType() == PSNodeType::FREE ||
-            n->getType() == PSNodeType::INVALIDATE_LOCALS)
-            return true;
-
-        return false;
-    }
-
-    bool beforeProcessed(PSNode *n) override
-    {
-        MemoryMapT *mm = n->getData<MemoryMapT>();
-        if (mm)
-            return false;
-
-        bool changed = false;
-
-        // on these nodes the memory map can change
-        if (canChangeMM(n)) { // root node
-            // FIXME: we're leaking the memory maps
-            mm = new MemoryMapT();
-        } else if (n->predecessorsNum() > 1) {
-            // this is a join node, create a new map and
-            // merge the predecessors to it
-            mm = new MemoryMapT();
-
-            // merge information from predecessors into the new map
-            // XXX: this is necessary also with the merge in afterProcess,
-            // because this copies the information even for single
-            // predecessor, whereas afterProcessed copies the
-            // information only for two or more predecessors
-            for (PSNode *p : n->getPredecessors()) {
-                MemoryMapT *pm = p->getData<MemoryMapT>();
-                // merge pm to mm (if pm was already created)
-                if (pm) {
-                    changed |= mergeMaps(mm, pm, nullptr);
-                }
-            }
-        } else {
-            // this node can not change the memory map,
-            // so just add a pointer from the predecessor
-            // to this map
-            PSNode *pred = n->getSinglePredecessor();
-            mm = pred->getData<MemoryMapT>();
-            assert(mm && "No memory map in the predecessor");
-        }
-
-        assert(mm && "Did not create the MM");
-
-        // memory map initialized, set it as data,
-        // so that we won't initialize it again
-        n->setData<MemoryMapT>(mm);
-
-        // ignore any changes here except when we merged some new information.
-        // The other changes we'll detect later
-        return changed;
-    }
+    : PointsToFlowSensitive(ps) {}
 
     bool afterProcessed(PSNode *n) override
     {
@@ -95,19 +45,16 @@ public:
         // FIXME: memcpy can be strong update too
         if (n->getType() == PSNodeType::STORE)
             strong_update = &n->getOperand(1)->pointsTo;
-
-        if (n->getType() == PSNodeType::FREE)
+        else if (n->getType() == PSNodeType::FREE)
             strong_update = &n->getOperand(0)->pointsTo;
-
-        if (n->getType() == PSNodeType::INVALIDATE_LOCALS)
+        else if (n->getType() == PSNodeType::INVALIDATE_LOCALS)
             strong_update = &n->pointsTo;
 
         // merge information from predecessors if there's
         // more of them (if there's just one predecessor
         // and this is not a store, the memory map couldn't
         // change, so we don't have to do that)
-        if (n->predecessorsNum() > 1 || strong_update
-            || n->getType() == PSNodeType::MEMCPY) {
+        if (needsMerge(n)) {
             for (PSNode *p : n->getPredecessors()) {
                 MemoryMapT *pm = p->getData<MemoryMapT>();
                 // merge pm to mm (but only if pm was already created)
@@ -120,49 +67,19 @@ public:
         return changed;
     }
 
-    void getMemoryObjects(PSNode *where, const Pointer& pointer,
-                          std::vector<MemoryObject *>& objects) override
-    {
-        MemoryMapT *mm= where->getData<MemoryMapT>();
-        assert(mm && "Node does not have memory map");
-
-        auto bounds = getObjectRange(mm, pointer);
-        for (MemoryMapT::iterator I = bounds.first; I != bounds.second; ++I) {
-            assert(I->first.target == pointer.target
-                    && "Bug in getObjectRange");
-
-            for (MemoryObject *mo : I->second)
-                objects.push_back(mo);
-        }
-
-        assert(bounds.second->first.target != pointer.target
-                && "Bug in getObjectRange");
-
-        // if we haven't found any memory object, but this psnode
-        // is a write to memory, create a new one, so that
-        // the write has something to write to
-        if (objects.empty() && canChangeMM(where)) {
-            MemoryObject *mo = new MemoryObject(pointer.target);
-            (*mm)[pointer].insert(mo);
-            objects.push_back(mo);
-        }
-    }
-   
-    
     void getMemoryObjectsPointingTo(PSNode *where, const Pointer& pointer,
                           std::vector<MemoryObject *>& objects) override
     {
-        MemoryMapT *mm= where->getData<MemoryMapT>();
+        MemoryMapT *mm = where->getData<MemoryMapT>();
         assert(mm && "Node does not have memory map");
 
         for (auto& I : *mm) {
-            for (MemoryObject *mo : I.second) {
-                for (auto& it : mo->pointsTo) {
-                    for (const auto& ptr : it.second) {
-                        if (ptr.target == pointer.target) {
-                            objects.push_back(mo);
-                            break;
-                        }
+            MemoryObject *mo = I.second.get();
+            for (auto& it : mo->pointsTo) {
+                for (const auto& ptr : it.second) {
+                    if (ptr.target == pointer.target) {
+                        objects.push_back(mo);
+                        break;
                     }
                 }
             }
@@ -172,64 +89,22 @@ public:
     void getLocalMemoryObjects(PSNode *where, 
                           std::vector<MemoryObject *>& objects) override
     {
-        MemoryMapT *mm= where->getData<MemoryMapT>();
+        MemoryMapT *mm = where->getData<MemoryMapT>();
         assert(mm && "Node does not have memory map");
 
         for (auto& I : *mm) {
-            for (MemoryObject *mo : I.second) {
-                for (auto& it : mo->pointsTo) {
-                    for (const auto& ptr : it.second) {
-                        if (!ptr.target->isHeap() &&
-                            !ptr.target->isGlobal() &&
-                            ptr.target->getParent() == where->getParent()) {
-                            objects.push_back(mo);
-                            break;
-                        }
+            MemoryObject *mo = I.second.get();
+            for (auto& it : mo->pointsTo) {
+                for (const auto& ptr : it.second) {
+                    if (!ptr.target->isHeap() &&
+                        !ptr.target->isGlobal() &&
+                        ptr.target->getParent() == where->getParent()) {
+                        objects.push_back(mo);
+                        break;
                     }
                 }
             }
         }
-    }
-
-
-protected:
-
-    PointsToWithInvalidate() = default;
-
-private:
-
-    static bool comp(const std::pair<const Pointer, MemoryObjectsSetT>& a,
-                     const std::pair<const Pointer, MemoryObjectsSetT>& b) {
-        return a.first.target < b.first.target;
-    }
-
-    ///
-    // get interator range for elements that have information
-    // about the ptr.target node (ignoring the offsets)
-    std::pair<MemoryMapT::iterator, MemoryMapT::iterator>
-    getObjectRange(MemoryMapT *mm, const Pointer& ptr) {
-        std::pair<const Pointer, MemoryObjectsSetT> what(ptr, MemoryObjectsSetT());
-        return std::equal_range(mm->begin(), mm->end(), what, comp);
-    }
-
-    ///
-    // Merge two Memory maps, return true if any new information was created,
-    // otherwise return false
-    bool mergeMaps(MemoryMapT *mm, MemoryMapT *pm,
-                   PointsToSetT *strong_update) {
-        bool changed = false;
-        for (auto& it : *pm) {
-            const Pointer& ptr = it.first;
-            if (strong_update && strong_update->count(ptr))
-                continue;
-
-            // use [] to create the object if needed
-            MemoryObjectsSetT& S = (*mm)[ptr];
-            for (auto& elem : it.second)
-                changed |= S.insert(elem).second;
-        }
-
-        return changed;
     }
 };
 
