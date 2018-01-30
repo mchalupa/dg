772c46e7d900920f5ae20a35a90cf12c6aad813c
Marek Chalupa <xchalup4@fi.muni.cz>
2018-01-30 17:22:44 +0100

    Offset.h: use using directive for the internal type
    So that we can write Offset::type instead of uint64_t.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- src/analysis/Offset.h
+++ src/analysis/Offset.h
@@ -11,7 +11,9 @@ namespace analysis {
 // maybe later we'll make it a range
 struct Offset
 {
-    Offset(uint64_t o = UNKNOWN_OFFSET) : offset(o) {}
+    using type = uint64_t;
+
+    Offset(type o = UNKNOWN_OFFSET) : offset(o) {}
     Offset& operator+=(const Offset& o)
     {
         if (offset == UNKNOWN_OFFSET)
@@ -43,17 +45,17 @@ struct Offset
         return offset == o.offset;
     }
 
-    bool inRange(uint64_t from, uint64_t to) const
+    bool inRange(type from, type to) const
     {
         return (offset >= from && offset <= to);
     }
 
     bool isUnknown() const { return offset == UNKNOWN_OFFSET; }
 
-    uint64_t operator*() const { return offset; }
-    const uint64_t *operator->() const { return &offset; }
+    type operator*() const { return offset; }
+    const type *operator->() const { return &offset; }
 
-    uint64_t offset;
+    type offset;
 };
 
 } // namespace analysis
