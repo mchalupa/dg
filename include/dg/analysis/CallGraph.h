9231813642cd3759b4011ba300b9efdbf39dc625
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-17 18:02:28 +0100

    PTA: compute call-graph separately

---
+++ include/dg/analysis/CallGraph.h
@@ -0,0 +1,73 @@
+#ifndef _DG_GENERIC_CALLGRAPH_H_
+#define _DG_GENERIC_CALLGRAPH_H_
+
+#include <map>
+#include <vector>
+
+template <typename ValueT>
+class GenericCallGraph {
+public:
+    class FuncNode {
+        std::vector<FuncNode *> _calls;
+        std::vector<FuncNode *> _callers;
+
+        template <typename Cont>
+        bool _contains(FuncNode *x, const Cont& C) const {
+            for (auto s : C) {
+                if (s == x)
+                    return true;
+            }
+            return false;
+        }
+
+    public:
+        ValueT value;
+
+        FuncNode(ValueT& nd) : value(nd) {};
+        FuncNode(FuncNode&&) = default;
+
+        bool calls(FuncNode *x) const { return _contains(x, _calls); }
+        bool isCalledBy(FuncNode *x) const { return _contains(x, _callers); }
+
+        bool addCall(FuncNode *x) {
+            if (calls(x))
+                return false;
+            _calls.push_back(x);
+            if (!x->isCalledBy(this))
+                x->_callers.push_back(this);
+            return true;
+        }
+    };
+
+    // a calls b
+    bool addCall(ValueT& a, ValueT& b) {
+        auto A = getOrCreate(a);
+        auto B = getOrCreate(b);
+        return A->addCall(B);
+    }
+
+private:
+
+    FuncNode *getOrCreate(ValueT& v) {
+        auto it = _mapping.find(v);
+        if (it == _mapping.end()) {
+            auto newIt = _mapping.emplace(v, FuncNode(v));
+            return &newIt.first->second;
+        }
+        return &it->second;
+    }
+
+    std::map<ValueT, FuncNode> _mapping;
+    //std::map<ValueT, FuncNode *> _mapping;
+    //std::vector<FuncNode> _nodes;
+
+public:
+    auto begin() -> decltype(_mapping.begin()) { return _mapping.begin(); }
+    auto end() -> decltype(_mapping.end()) { return _mapping.end(); }
+    auto begin() const -> decltype(_mapping.begin()) { return _mapping.begin(); }
+    auto end() const -> decltype(_mapping.end()) { return _mapping.end(); }
+
+
+};
+
+#endif // _DG_GENERIC_CALLGRAPH_H_
