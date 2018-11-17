2f8c5b99cf3fc43985e277bd8d1b07098ec8119d
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-17 13:20:31 +0100

    VR: split code into several files
    The code blowed up.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

---
+++ include/dg/llvm/analysis/ValueRelations/Relations.h
@@ -0,0 +1,270 @@
+#ifndef _DG_LLVM_RELATIONS_H_
+#define _DG_LLVM_RELATIONS_H_
+
+#include <memory>
+#include <cassert>
+
+#ifndef NDEBUG
+#include "getValName.h"
+#endif
+
+namespace llvm {
+    class Value;
+}
+
+namespace dg {
+
+/// --------------------------------------------------- ///
+// Relation
+/// --------------------------------------------------- ///
+class Relations;
+class VRRelation {
+    enum VRRelationType {
+        NONE = 0, EQ = 1, NEQ = 2, LE = 3, LT = 4, GE = 5, GT = 6
+    } _relation{VRRelationType::NONE};
+
+    const llvm::Value *_lhs{nullptr}, *_rhs{nullptr};
+
+    VRRelation(VRRelationType type,
+               const llvm::Value *lhs, const llvm::Value *rhs)
+    : _relation(type), _lhs(lhs), _rhs(rhs) {}
+
+    auto getRelation() const -> decltype(_relation) { return _relation; }
+
+public:
+
+	VRRelation() = default;
+
+#ifndef NDEBUG
+    void dump() const {
+        std::cout << "(" << debug::getValName(_lhs);
+
+        switch(_relation) {
+        case VRRelationType::EQ: std::cout << " = "; break;
+        case VRRelationType::NEQ: std::cout << " != "; break;
+        case VRRelationType::LT: std::cout << " < "; break;
+        case VRRelationType::LE: std::cout << " <= "; break;
+        case VRRelationType::GT: std::cout << " > "; break;
+        case VRRelationType::GE: std::cout << " >= "; break;
+        default: abort();
+        }
+        std::cout << debug::getValName(_rhs) << ")";
+    }
+#endif
+
+    bool operator<(const VRRelation& oth) const {
+        return _lhs == oth._lhs ?
+                _rhs < oth._rhs : _lhs < oth._lhs;
+    }
+
+    bool isEq() const { return _relation == VRRelationType::EQ; }
+    bool isNeq() const { return _relation == VRRelationType::NEQ; }
+    bool isLt() const { return _relation == VRRelationType::LE; }
+    bool isLe() const { return _relation == VRRelationType::LT; }
+    bool isGt() const { return _relation == VRRelationType::GT; }
+    bool isGe() const { return _relation == VRRelationType::GE; }
+
+    auto getLHS() const -> decltype(_lhs) { return _lhs; }
+    auto getRHS() const -> decltype(_rhs) { return _rhs; }
+
+	static VRRelation Eq(const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(VRRelationType::EQ, l, r);
+	}
+	static VRRelation Neq(const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(VRRelationType::NEQ, l, r);
+	}
+	static VRRelation Lt(const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(VRRelationType::LT, l, r);
+	}
+	static VRRelation Le(const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(VRRelationType::LE, l, r);
+	}
+	static VRRelation Gt(const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(VRRelationType::GT, l, r);
+	}
+	static VRRelation Ge(const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(VRRelationType::GE, l, r);
+	}
+	static VRRelation sameOp(const VRRelation& rel, const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(rel._relation, l, r);
+	}
+
+	static VRRelation Not(const VRRelation& rel) {
+        switch(rel._relation) {
+        case VRRelationType::EQ:  return Neq(rel._lhs, rel._rhs);
+        case VRRelationType::NEQ: return Eq(rel._lhs, rel._rhs);
+        case VRRelationType::LT:  return Ge(rel._lhs, rel._rhs);
+        case VRRelationType::LE:  return Gt(rel._lhs, rel._rhs);
+        case VRRelationType::GT:  return Le(rel._lhs, rel._rhs);
+        case VRRelationType::GE:  return Lt(rel._lhs, rel._rhs);
+        default: abort();
+        }
+	}
+
+	static VRRelation revert(const VRRelation& rel) {
+        switch(rel._relation) {
+        case VRRelationType::EQ:  return Eq(rel._rhs, rel._lhs);
+        case VRRelationType::NEQ: return Neq(rel._rhs, rel._lhs);
+        case VRRelationType::LT:  return Gt(rel._rhs, rel._lhs);
+        case VRRelationType::LE:  return Ge(rel._rhs, rel._lhs);
+        case VRRelationType::GT:  return Lt(rel._rhs, rel._lhs);
+        case VRRelationType::GE:  return Le(rel._rhs, rel._lhs);
+        default: abort();
+        }
+	}
+
+	friend VRRelation Eq(const llvm::Value *, const llvm::Value *);
+	friend VRRelation Neq(const llvm::Value *, const llvm::Value *);
+	friend VRRelation Lt(const llvm::Value *, const llvm::Value *);
+	friend VRRelation Le(const llvm::Value *, const llvm::Value *);
+	friend VRRelation Gt(const llvm::Value *, const llvm::Value *);
+	friend VRRelation Ge(const llvm::Value *, const llvm::Value *);
+	friend VRRelation Not(const VRRelation& r);
+	friend VRRelation revert(const VRRelation&);
+	friend VRRelation sameOp(const VRRelation&, const llvm::Value *, const llvm::Value *);
+    friend class Relations;
+};
+
+/// Set of relations for one value
+class Relations {
+    // this value is in relation with values in rhs
+    const llvm::Value *value;
+    // one set for each type of relation
+    std::set<const llvm::Value *> rhs[7]{{}};
+
+public:
+    Relations(const llvm::Value *v) : value(v) {}
+
+    bool add(const VRRelation& rel) {
+        assert(rel.getRelation() != VRRelation::VRRelationType::NONE);
+        assert(rel.getLHS() == value);
+        return rhs[rel.getRelation()].insert(rel.getRHS()).second;
+    }
+
+    bool add(const Relations& oth) {
+        bool changed = false;
+        for (int i = 0; i < 7; ++i) {
+            for (auto& it : oth.rhs[i])
+                changed |= rhs[i].insert(it).second;
+        }
+
+        return changed;
+    }
+
+    struct const_iterator {
+        unsigned idx;
+        const Relations& relations;
+        std::set<const llvm::Value *>::const_iterator it;
+
+        const_iterator(const Relations& r, unsigned idx)
+        : idx(idx), relations(r) {
+            if (idx < 7)
+                it = relations.rhs[idx].begin();
+        }
+
+        VRRelation operator*() const {
+            return VRRelation(static_cast<VRRelation::VRRelationType>(idx),
+                              relations.value, *it);
+        }
+
+        const_iterator& operator++() {
+            ++it;
+            if (it == relations.rhs[idx].end())
+                ++idx;
+            if (idx < 7)
+                it = relations.rhs[idx].begin();
+            return *this;
+        }
+
+        bool operator==(const const_iterator& rhs) const {
+            if (idx < 7) {
+                return idx == rhs.idx ? it == rhs.it : false;
+            }
+
+            return idx == rhs.idx;
+        }
+
+        bool operator!=(const const_iterator& rhs) const { return !operator==(rhs); }
+    };
+
+    const_iterator begin() const { return const_iterator(*this, 1); }
+    const_iterator end() const { return const_iterator(*this, 7); }
+
+
+#ifndef NDEBUG
+    void dump() const {
+        for (int i = 1; i < 7; ++i) {
+            for (const auto& r : rhs[i]) {
+                VRRelation(static_cast<VRRelation::VRRelationType>(i),
+                           value, r).dump();
+            }
+        }
+    }
+#endif // NDEBUG
+
+};
+
+class RelationsMap {
+    std::map<const llvm::Value *, Relations> relations;
+
+public:
+    RelationsMap() = default;
+    RelationsMap(const RelationsMap&) = default;
+
+    bool add(const VRRelation& rel) {
+        auto it = relations.find(rel.getLHS());
+        if (it == relations.end()) {
+            auto n = relations.emplace(std::piecewise_construct,
+                                       std::forward_as_tuple(rel.getLHS()),
+                                       std::forward_as_tuple(rel.getLHS()));
+            return n.first->second.add(rel);
+        }
+
+        return it->second.add(rel);
+    }
+
+	bool add(const RelationsMap& rhs) {
+		bool changed = false;
+		for (const auto& it : rhs) {
+            auto my = relations.find(it.first);
+            if (my == relations.end()) {
+                auto myNew = relations.emplace(std::piecewise_construct,
+                                               std::forward_as_tuple(it.first),
+                                               std::forward_as_tuple(it.first));
+                changed |= myNew.first->second.add(it.second);
+                continue;
+            }
+			changed |= my->second.add(it.second);
+		}
+		return changed;
+	}
+
+    /*
+	void intersect(const RelationsMap& rhs) {
+		decltype(relations) tmp;
+		for (const auto& r : rhs) {
+			if (relations.count(r) > 0)
+				tmp.insert(r);
+		}
+		relations.swap(tmp);
+	}
+    */
+
+    auto begin() -> decltype(relations.begin()) { return relations.begin(); }
+    auto end() -> decltype(relations.end()) { return relations.end(); }
+    auto begin() const -> decltype(relations.begin()) { return relations.begin(); }
+    auto end() const -> decltype(relations.end()) { return relations.end(); }
+
+#ifndef NDEBUG
+    void dump() const {
+        std::cout << "{";
+        for (auto& it : relations)
+            it.second.dump();
+        std::cout << "}";
+    }
+#endif // NDEBUG
+};
+
+} // namespace dg
+
+#endif // _DG_LLVM_RELATIONS_H_
--- include/dg/llvm/analysis/ValueRelations/ValueRelations.h
+++ include/dg/llvm/analysis/ValueRelations/ValueRelations.h
@@ -3,36 +3,24 @@
 
 #include <list>
 
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <string>
-
 #include <llvm/IR/Value.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/CFG.h>
-#include <llvm/Support/raw_os_ostream.h>
 
 #include "dg/analysis/ValueRelations/ValueRelations.h"
 
+#include "Relations.h"
+#include "EqualityMap.h"
+#include "ReadsMap.h"
+
+#ifndef NDEBUG
+#include "getValName.h"
+#endif
+
 namespace dg {
 namespace analysis {
-
-namespace debug {
-static inline std::string getValName(const llvm::Value *val) {
-    std::ostringstream ostr;
-    llvm::raw_os_ostream ro(ostr);
-
-    assert(val);
-    ro << *val;
-    ro.flush();
-
-    // break the string if it is too long
-    return ostr.str();
-}
-} // namespace debug
 
 class VROp {
 protected:
@@ -103,118 +91,6 @@ struct VRAssume : public VROp {
 #endif
 };
 
-/// --------------------------------------------------- ///
-// Relation
-/// --------------------------------------------------- ///
-class Relations;
-class VRRelation {
-    enum VRRelationType {
-        NONE = 0, EQ = 1, NEQ = 2, LE = 3, LT = 4, GE = 5, GT = 6
-    } _relation{VRRelationType::NONE};
-
-    const llvm::Value *_lhs{nullptr}, *_rhs{nullptr};
-
-    VRRelation(VRRelationType type,
-               const llvm::Value *lhs, const llvm::Value *rhs)
-    : _relation(type), _lhs(lhs), _rhs(rhs) {}
-
-    auto getRelation() const -> decltype(_relation) { return _relation; }
-
-public:
-
-	VRRelation() = default;
-
-#ifndef NDEBUG
-    void dump() const {
-        std::cout << "(" << debug::getValName(_lhs);
-
-        switch(_relation) {
-        case VRRelationType::EQ: std::cout << " = "; break;
-        case VRRelationType::NEQ: std::cout << " != "; break;
-        case VRRelationType::LT: std::cout << " < "; break;
-        case VRRelationType::LE: std::cout << " <= "; break;
-        case VRRelationType::GT: std::cout << " > "; break;
-        case VRRelationType::GE: std::cout << " >= "; break;
-        default: abort();
-        }
-        std::cout << debug::getValName(_rhs) << ")";
-    }
-#endif
-
-    bool operator<(const VRRelation& oth) const {
-        return _lhs == oth._lhs ?
-                _rhs < oth._rhs : _lhs < oth._lhs;
-    }
-
-    bool isEq() const { return _relation == VRRelationType::EQ; }
-    bool isNeq() const { return _relation == VRRelationType::NEQ; }
-    bool isLt() const { return _relation == VRRelationType::LE; }
-    bool isLe() const { return _relation == VRRelationType::LT; }
-    bool isGt() const { return _relation == VRRelationType::GT; }
-    bool isGe() const { return _relation == VRRelationType::GE; }
-
-    auto getLHS() const -> decltype(_lhs) { return _lhs; }
-    auto getRHS() const -> decltype(_rhs) { return _rhs; }
-
-	static VRRelation Eq(const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(VRRelationType::EQ, l, r);
-	}
-	static VRRelation Neq(const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(VRRelationType::NEQ, l, r);
-	}
-	static VRRelation Lt(const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(VRRelationType::LT, l, r);
-	}
-	static VRRelation Le(const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(VRRelationType::LE, l, r);
-	}
-	static VRRelation Gt(const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(VRRelationType::GT, l, r);
-	}
-	static VRRelation Ge(const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(VRRelationType::GE, l, r);
-	}
-	static VRRelation sameOp(const VRRelation& rel, const llvm::Value *l, const llvm::Value *r) {
-		return VRRelation(rel._relation, l, r);
-	}
-
-	static VRRelation Not(const VRRelation& rel) {
-        switch(rel._relation) {
-        case VRRelationType::EQ:  return Neq(rel._lhs, rel._rhs);
-        case VRRelationType::NEQ: return Eq(rel._lhs, rel._rhs);
-        case VRRelationType::LT:  return Ge(rel._lhs, rel._rhs);
-        case VRRelationType::LE:  return Gt(rel._lhs, rel._rhs);
-        case VRRelationType::GT:  return Le(rel._lhs, rel._rhs);
-        case VRRelationType::GE:  return Lt(rel._lhs, rel._rhs);
-        default: abort();
-        }
-	}
-
-	static VRRelation revert(const VRRelation& rel) {
-        switch(rel._relation) {
-        case VRRelationType::EQ:  return Eq(rel._rhs, rel._lhs);
-        case VRRelationType::NEQ: return Neq(rel._rhs, rel._lhs);
-        case VRRelationType::LT:  return Gt(rel._rhs, rel._lhs);
-        case VRRelationType::LE:  return Ge(rel._rhs, rel._lhs);
-        case VRRelationType::GT:  return Lt(rel._rhs, rel._lhs);
-        case VRRelationType::GE:  return Le(rel._rhs, rel._lhs);
-        default: abort();
-        }
-	}
-
-	friend VRRelation Eq(const llvm::Value *, const llvm::Value *);
-	friend VRRelation Neq(const llvm::Value *, const llvm::Value *);
-	friend VRRelation Lt(const llvm::Value *, const llvm::Value *);
-	friend VRRelation Le(const llvm::Value *, const llvm::Value *);
-	friend VRRelation Gt(const llvm::Value *, const llvm::Value *);
-	friend VRRelation Ge(const llvm::Value *, const llvm::Value *);
-	friend VRRelation Not(const VRRelation& r);
-	friend VRRelation revert(const VRRelation&);
-	friend VRRelation sameOp(const VRRelation&, const llvm::Value *, const llvm::Value *);
-    friend class Relations;
-};
-
-
 struct VRLocation;
 struct VREdge {
     VRLocation *source;
@@ -226,359 +102,6 @@ struct VREdge {
     : source(s), target(t), op(std::move(op)) {}
 };
 
-template <typename T>
-class EqualityMap {
-    struct _Cmp {
-        bool operator()(const llvm::Value *a, const llvm::Value *b) const {
-            // XXX: merge constants?
-            return a < b;
-        }
-    };
-
-    using SetT = std::set<T, _Cmp>;
-    using ClassT = std::shared_ptr<SetT>;
-    std::map<T, ClassT, _Cmp> _map;
-
-    // FIXME: use variadic templates
-    ClassT newClass(const T& a, const T& b) {
-        auto cls = ClassT(new SetT());
-        cls->insert(a);
-        cls->insert(b);
-        return cls;
-    }
-
-    ClassT newClass(const T& a) {
-        auto cls = ClassT(new SetT());
-        cls->insert(a);
-        return cls;
-    }
-
-public:
-    bool add(const T& a, const T& b) {
-        auto itA = _map.find(a);
-        auto itB = _map.find(b);
-        if (itA == _map.end()) {
-            if (itB == _map.end()) {
-                if (a == b) {
-                    auto newcls = newClass(a);
-                    _map[a] = newcls;
-                    assert(newcls.use_count() == 2);
-                } else {
-                    auto newcls = newClass(a, b);
-                    _map[b] = newcls;
-                    _map[a] = newcls;
-                    assert(newcls.use_count() == 3);
-                }
-            } else {
-                auto B = itB->second;
-                B->insert(a);
-                _map[a] = B;
-            }
-        } else {
-            auto A = itA->second;
-            if (itB == _map.end()) {
-                A->insert(b);
-                _map[b] = A;
-            } else {
-                // merge of classes
-                auto B = itB->second;
-                if (A == B)
-                    return false;
-
-                for (auto& val : *B.get()) {
-                    A->insert(val);
-                    _map[val] = A;
-                }
-                assert(B.use_count() == 1);
-                A->insert(b);
-                assert(_map[b] == A);
-            }
-        }
-
-        assert(!_map.empty());
-        assert(get(a) != nullptr);
-        assert(get(a) == get(b));
-        assert(get(a)->count(a) > 0);
-        assert(get(a)->count(b) > 0);
-        return true;
-    }
-
-    bool add(const EqualityMap& rhs) {
-        bool changed = false;
-        // FIXME: not very efficient
-        for (auto& it : rhs._map) {
-            for (auto& eq : *it.second.get()) {
-                changed |= add(it.first, eq);
-            }
-        }
-
-        return changed;
-    }
-
-    bool add(const T& a, const SetT& S) {
-        bool changed = false;
-        for (auto eq : S) {
-            changed |= add(a, eq);
-        }
-
-        return changed;
-    }
-
-    SetT *get(const T& a) {
-        auto it = _map.find(a);
-        if (it == _map.end()) {
-            return nullptr;
-        }
-        return it->second.get();
-    }
-
-    void intersect(const EqualityMap& rhs) {
-        EqualityMap tmp;
-        for (auto& it : rhs) {
-            auto ourS = get(it.first);
-            if (!ourS)
-                continue;
-
-            for (auto x : *ourS) {
-                if (it.second->count(x) > 0)
-                    tmp.add(it.first, x);
-            }
-        }
-
-        _map.swap(tmp._map);
-    }
-
-    auto begin() -> decltype(_map.begin()) { return _map.begin(); }
-    auto end() -> decltype(_map.end()) { return _map.end(); }
-    auto begin() const -> decltype(_map.begin()) { return _map.begin(); }
-    auto end() const -> decltype(_map.end()) { return _map.end(); }
-
-#ifndef NDEBUG
-    void dump() const {
-        std::set<SetT*> classes;
-        for (const auto& it : _map) {
-            classes.insert(it.second.get());
-        }
-
-        if (classes.empty()) {
-            return;
-        }
-
-        for (const auto cls : classes) {
-            std::cout << "{";
-            int t = 0;
-            for (const auto& val : *cls) {
-                if (++t > 1)
-                    std::cout << " = ";
-                std::cout << debug::getValName(val);
-            }
-            std::cout << "}\n";
-        }
-        std::cout << std::endl;
-    }
-#endif
-};
-
-class ReadsMap {
-    // pair (a,b) such that b = load a in the future
-    std::map<const llvm::Value *, const llvm::Value *> _map;
-
-public:
-    auto begin() -> decltype(_map.begin()) { return _map.begin(); }
-    auto end() -> decltype(_map.end()) { return _map.end(); }
-    auto begin() const -> decltype(_map.begin()) { return _map.begin(); }
-    auto end() const -> decltype(_map.end()) { return _map.end(); }
-
-    bool add(const llvm::Value *from, const llvm::Value *val) {
-        assert(val != nullptr);
-        auto it = _map.find(from);
-        if (it == _map.end()) {
-            _map.emplace_hint(it, from, val);
-            return true;
-        } else if (it->second == val) {
-            return false;
-        }
-
-        // XXX: use the found iterator
-        _map[from] = val;
-        return true;
-    }
-
-	bool add(const ReadsMap& rhs) {
-		bool changed = false;
-		for (const auto& it : rhs) {
-			assert(get(it.first) == nullptr || get(it.first) == it.second);
-			changed |= add(it.first, it.second);
-		}
-		return changed;
-	}
-
-    const llvm::Value *get(const llvm::Value *from) const {
-        auto it = _map.find(from);
-        if (it == _map.end())
-            return nullptr;
-        return it->second;
-    }
-
-    void intersect(const ReadsMap& rhs) {
-        decltype(_map) tmp;
-        for (auto& it : rhs._map) {
-            if (get(it.first) == it.second)
-                tmp.emplace(it.first, it.second);
-        }
-
-        _map.swap(tmp);
-    }
-
-#ifndef NDEBUG
-    void dump() const {
-        for (auto& it : _map) {
-            std::cout << "L(" << debug::getValName(it.first) << ") = "
-                      << debug::getValName(it.second) << "\n";
-        }
-    }
-#endif // NDEBUG
-};
-
-class Relations {
-    // this value is in relation with values in rhs
-    const llvm::Value *value;
-    // one set for each type of relation
-    std::set<const llvm::Value *> rhs[7]{{}};
-
-public:
-    Relations(const llvm::Value *v) : value(v) {}
-
-    bool add(const VRRelation& rel) {
-        assert(rel.getRelation() != VRRelation::VRRelationType::NONE);
-        assert(rel.getLHS() == value);
-        return rhs[rel.getRelation()].insert(rel.getRHS()).second;
-    }
-
-    bool add(const Relations& oth) {
-        bool changed = false;
-        for (int i = 0; i < 7; ++i) {
-            for (auto& it : oth.rhs[i])
-                changed |= rhs[i].insert(it).second;
-        }
-
-        return changed;
-    }
-
-    struct const_iterator {
-        unsigned idx;
-        const Relations& relations;
-        std::set<const llvm::Value *>::const_iterator it;
-
-        const_iterator(const Relations& r, unsigned idx)
-        : idx(idx), relations(r) {
-            if (idx < 7)
-                it = relations.rhs[idx].begin();
-        }
-
-        VRRelation operator*() const {
-            return VRRelation(static_cast<VRRelation::VRRelationType>(idx),
-                              relations.value, *it);
-        }
-
-        const_iterator& operator++() {
-            ++it;
-            if (it == relations.rhs[idx].end())
-                ++idx;
-            if (idx < 7)
-                it = relations.rhs[idx].begin();
-            return *this;
-        }
-
-        bool operator==(const const_iterator& rhs) const {
-            if (idx < 7) {
-                return idx == rhs.idx ? it == rhs.it : false;
-            }
-
-            return idx == rhs.idx;
-        }
-
-        bool operator!=(const const_iterator& rhs) const { return !operator==(rhs); }
-    };
-
-    const_iterator begin() const { return const_iterator(*this, 1); }
-    const_iterator end() const { return const_iterator(*this, 7); }
-
-
-#ifndef NDEBUG
-    void dump() const {
-        for (int i = 1; i < 7; ++i) {
-            for (const auto& r : rhs[i]) {
-                VRRelation(static_cast<VRRelation::VRRelationType>(i),
-                           value, r).dump();
-            }
-        }
-    }
-#endif // NDEBUG
-
-};
-
-class RelationsSet {
-    std::map<const llvm::Value *, Relations> relations;
-
-public:
-    RelationsSet() = default;
-    RelationsSet(const RelationsSet&) = default;
-
-    bool add(const VRRelation& rel) {
-        auto it = relations.find(rel.getLHS());
-        if (it == relations.end()) {
-            auto n = relations.emplace(std::piecewise_construct,
-                                       std::forward_as_tuple(rel.getLHS()),
-                                       std::forward_as_tuple(rel.getLHS()));
-            return n.first->second.add(rel);
-        }
-
-        return it->second.add(rel);
-    }
-
-	bool add(const RelationsSet& rhs) {
-		bool changed = false;
-		for (const auto& it : rhs) {
-            auto my = relations.find(it.first);
-            if (my == relations.end()) {
-                auto myNew = relations.emplace(std::piecewise_construct,
-                                               std::forward_as_tuple(it.first),
-                                               std::forward_as_tuple(it.first));
-                changed |= myNew.first->second.add(it.second);
-                continue;
-            }
-			changed |= my->second.add(it.second);
-		}
-		return changed;
-	}
-
-    /*
-	void intersect(const RelationsSet& rhs) {
-		decltype(relations) tmp;
-		for (const auto& r : rhs) {
-			if (relations.count(r) > 0)
-				tmp.insert(r);
-		}
-		relations.swap(tmp);
-	}
-    */
-
-    auto begin() -> decltype(relations.begin()) { return relations.begin(); }
-    auto end() -> decltype(relations.end()) { return relations.end(); }
-    auto begin() const -> decltype(relations.begin()) { return relations.begin(); }
-    auto end() const -> decltype(relations.end()) { return relations.end(); }
-
-#ifndef NDEBUG
-    void dump() const {
-        std::cout << "{";
-        for (auto& it : relations)
-            it.second.dump();
-        std::cout << "}";
-    }
-#endif // NDEBUG
-};
-
 struct VRLocation  {
     const unsigned id;
 
@@ -587,7 +110,7 @@ struct VRLocation  {
     // pairs (a,b) such that if we meet "load a", we know
     // the result is b
     ReadsMap reads;
-    RelationsSet relations;
+    RelationsMap relations;
 
     std::vector<VREdge *> predecessors{};
     std::vector<std::unique_ptr<VREdge>> successors{};
@@ -599,7 +122,7 @@ struct VRLocation  {
 
     /*
     bool _addTransitive() {
-        RelationsSet tmp = relations;
+        RelationsMap tmp = relations;
         bool changed = false;
         for (auto& r1 : relations) {
             for (auto& r2 : relations) {
@@ -754,7 +277,7 @@ class LLVMValueRelationsAnalysis {
 
     bool instructionGen(const llvm::Instruction *I,
                         EqualityMap<const llvm::Value*>& E,
-                        RelationsSet& Rel,
+                        RelationsMap& Rel,
                         ReadsMap& R, VRLocation *source) {
         using namespace llvm;
         if (auto SI = dyn_cast<StoreInst>(I)) {
@@ -804,7 +327,7 @@ class LLVMValueRelationsAnalysis {
     }
 
     bool assumeGen(VRAssume *assume,
-				   RelationsSet& Rel,
+				   RelationsMap& Rel,
                    EqualityMap<const llvm::Value*>& E,
                    VRLocation *source) {
 		using namespace llvm;
@@ -845,7 +368,7 @@ class LLVMValueRelationsAnalysis {
     // and store it in E and R
     bool collect(VRLocation *loc,
                  EqualityMap<const llvm::Value*>& E,
-				 RelationsSet& Rel,
+				 RelationsMap& Rel,
                  ReadsMap& R,
                  VREdge *edge) {
         auto source = edge->source;
@@ -975,7 +498,9 @@ public:
             }
         } while (changed);
 
+#ifndef NDEBUG
         llvm::errs() << "Number of iterations: " << n << "\n";
+#endif
     }
 
     LLVMValueRelationsAnalysis(const llvm::Module *M) : _M(M) {
@@ -1051,7 +576,9 @@ class LLVMValueRelations {
             auto br = llvm::dyn_cast<llvm::BranchInst>(term);
             if (!br) {
                 if (llvm::succ_begin(&B) != llvm::succ_end(&B)) {
+#ifndef NDEBUG
                     llvm::errs() << "Unhandled terminator: " << *term << "\n";
+#endif
                     abort();
                 }
                 continue; // no successor
