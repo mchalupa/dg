d0daeac5e04903fed6c02d06f170ed66ebf63370
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-17 13:01:46 +0100

    VR: add a different representation of RelationsSet

--- include/dg/llvm/analysis/ValueRelations/ValueRelations.h
+++ include/dg/llvm/analysis/ValueRelations/ValueRelations.h
@@ -106,6 +106,7 @@ struct VRAssume : public VROp {
 /// --------------------------------------------------- ///
 // Relation
 /// --------------------------------------------------- ///
+class Relations;
 class VRRelation {
     enum VRRelationType {
         NONE = 0, EQ = 1, NEQ = 2, LE = 3, LT = 4, GE = 5, GT = 6
@@ -116,6 +117,8 @@ class VRRelation {
     VRRelation(VRRelationType type,
                const llvm::Value *lhs, const llvm::Value *rhs)
     : _relation(type), _lhs(lhs), _rhs(rhs) {}
+
+    auto getRelation() const -> decltype(_relation) { return _relation; }
 
 public:
 
@@ -171,6 +174,9 @@ public:
 	static VRRelation Ge(const llvm::Value *l, const llvm::Value *r) {
 		return VRRelation(VRRelationType::GE, l, r);
 	}
+	static VRRelation sameOp(const VRRelation& rel, const llvm::Value *l, const llvm::Value *r) {
+		return VRRelation(rel._relation, l, r);
+	}
 
 	static VRRelation Not(const VRRelation& rel) {
         switch(rel._relation) {
@@ -184,6 +190,18 @@ public:
         }
 	}
 
+	static VRRelation revert(const VRRelation& rel) {
+        switch(rel._relation) {
+        case VRRelationType::EQ:  return Eq(rel._rhs, rel._lhs);
+        case VRRelationType::NEQ: return Neq(rel._rhs, rel._lhs);
+        case VRRelationType::LT:  return Gt(rel._rhs, rel._lhs);
+        case VRRelationType::LE:  return Ge(rel._rhs, rel._lhs);
+        case VRRelationType::GT:  return Lt(rel._rhs, rel._lhs);
+        case VRRelationType::GE:  return Le(rel._rhs, rel._lhs);
+        default: abort();
+        }
+	}
+
 	friend VRRelation Eq(const llvm::Value *, const llvm::Value *);
 	friend VRRelation Neq(const llvm::Value *, const llvm::Value *);
 	friend VRRelation Lt(const llvm::Value *, const llvm::Value *);
@@ -191,6 +209,9 @@ public:
 	friend VRRelation Gt(const llvm::Value *, const llvm::Value *);
 	friend VRRelation Ge(const llvm::Value *, const llvm::Value *);
 	friend VRRelation Not(const VRRelation& r);
+	friend VRRelation revert(const VRRelation&);
+	friend VRRelation sameOp(const VRRelation&, const llvm::Value *, const llvm::Value *);
+    friend class Relations;
 };
 
 
@@ -419,22 +440,120 @@ public:
 #endif // NDEBUG
 };
 
-class RelationsSet {
-    std::set<VRRelation> relations;
+class Relations {
+    // this value is in relation with values in rhs
+    const llvm::Value *value;
+    // one set for each type of relation
+    std::set<const llvm::Value *> rhs[7]{{}};
 
 public:
+    Relations(const llvm::Value *v) : value(v) {}
+
     bool add(const VRRelation& rel) {
-        return relations.insert(rel).second;
+        assert(rel.getRelation() != VRRelation::VRRelationType::NONE);
+        assert(rel.getLHS() == value);
+        return rhs[rel.getRelation()].insert(rel.getRHS()).second;
+    }
+
+    bool add(const Relations& oth) {
+        bool changed = false;
+        for (int i = 0; i < 7; ++i) {
+            for (auto& it : oth.rhs[i])
+                changed |= rhs[i].insert(it).second;
+        }
+
+        return changed;
+    }
+
+    struct const_iterator {
+        unsigned idx;
+        const Relations& relations;
+        std::set<const llvm::Value *>::const_iterator it;
+
+        const_iterator(const Relations& r, unsigned idx)
+        : idx(idx), relations(r) {
+            if (idx < 7)
+                it = relations.rhs[idx].begin();
+        }
+
+        VRRelation operator*() const {
+            return VRRelation(static_cast<VRRelation::VRRelationType>(idx),
+                              relations.value, *it);
+        }
+
+        const_iterator& operator++() {
+            ++it;
+            if (it == relations.rhs[idx].end())
+                ++idx;
+            if (idx < 7)
+                it = relations.rhs[idx].begin();
+            return *this;
+        }
+
+        bool operator==(const const_iterator& rhs) const {
+            if (idx < 7) {
+                return idx == rhs.idx ? it == rhs.it : false;
+            }
+
+            return idx == rhs.idx;
+        }
+
+        bool operator!=(const const_iterator& rhs) const { return !operator==(rhs); }
+    };
+
+    const_iterator begin() const { return const_iterator(*this, 1); }
+    const_iterator end() const { return const_iterator(*this, 7); }
+
+
+#ifndef NDEBUG
+    void dump() const {
+        for (int i = 1; i < 7; ++i) {
+            for (const auto& r : rhs[i]) {
+                VRRelation(static_cast<VRRelation::VRRelationType>(i),
+                           value, r).dump();
+            }
+        }
+    }
+#endif // NDEBUG
+
+};
+
+class RelationsSet {
+    std::map<const llvm::Value *, Relations> relations;
+
+public:
+    RelationsSet() = default;
+    RelationsSet(const RelationsSet&) = default;
+
+    bool add(const VRRelation& rel) {
+        auto it = relations.find(rel.getLHS());
+        if (it == relations.end()) {
+            auto n = relations.emplace(std::piecewise_construct,
+                                       std::forward_as_tuple(rel.getLHS()),
+                                       std::forward_as_tuple(rel.getLHS()));
+            return n.first->second.add(rel);
+        }
+
+        return it->second.add(rel);
     }
 
 	bool add(const RelationsSet& rhs) {
 		bool changed = false;
 		for (const auto& it : rhs) {
-			changed |= add(it);
+            auto my = relations.find(it.first);
+            if (my == relations.end()) {
+                auto myNew = relations.emplace(std::piecewise_construct,
+                                               std::forward_as_tuple(it.first),
+                                               std::forward_as_tuple(it.first));
+                changed |= myNew.first->second.add(it.second);
+                continue;
+            }
+			changed |= my->second.add(it.second);
 		}
 		return changed;
 	}
 
+    /*
 	void intersect(const RelationsSet& rhs) {
 		decltype(relations) tmp;
 		for (const auto& r : rhs) {
@@ -443,6 +562,7 @@ public:
 		}
 		relations.swap(tmp);
 	}
+    */
 
     auto begin() -> decltype(relations.begin()) { return relations.begin(); }
     auto end() -> decltype(relations.end()) { return relations.end(); }
@@ -452,9 +572,8 @@ public:
 #ifndef NDEBUG
     void dump() const {
         std::cout << "{";
-        for (const auto& it : relations) {
-            it.dump();
-        }
+        for (auto& it : relations)
+            it.second.dump();
         std::cout << "}";
     }
 #endif // NDEBUG
@@ -477,6 +596,22 @@ struct VRLocation  {
         edge->target->predecessors.push_back(edge.get());
         successors.emplace_back(std::move(edge));
     }
+
+    /*
+    bool _addTransitive() {
+        RelationsSet tmp = relations;
+        bool changed = false;
+        for (auto& r1 : relations) {
+            for (auto& r2 : relations) {
+                if (r1.isEq()) {
+                    if (r2.isLt() || r2.isLe())
+                    changed |= tmp.add(VRRelation::sameOp(r2, r1.getRHS()
+                }
+            }
+        }
+
+    }
+    */
 
     VRLocation(unsigned _id) : id(_id) {}
 
@@ -807,13 +942,16 @@ class LLVMValueRelationsAnalysis {
             }
 
             for (auto& it : pred->source->relations) {
-                auto LI = dyn_cast<LoadInst>(it.getLHS());
+                auto LI = dyn_cast<LoadInst>(it.first);
                 // XXX: we can do the same with constants
                 if (LI && fixedMemory.count(LI->getOperand(0)) > 0) {
-                    auto LI2 = dyn_cast<LoadInst>(it.getRHS());
-                    if (LI2 && !fixedMemory.count(LI2->getOperand(0)))
-                        continue;
-                    changed |= loc->relations.add(it);
+                    for (const auto& R : it.second) {
+                        assert(R.getLHS() == it.first);
+                        auto LI2 = dyn_cast<LoadInst>(R.getRHS());
+                        if (LI2 && !fixedMemory.count(LI2->getOperand(0)))
+                            continue;
+                        changed |= loc->relations.add(R);
+                    }
                 }
             }
         }
@@ -973,6 +1111,10 @@ public:
 
     decltype(_blocks) const& getBlocks() const {
         return _blocks;
+    }
+
+    bool isLt(const llvm::Value *a, const llvm::Value *b) {
+        abort();
     }
 };
 
