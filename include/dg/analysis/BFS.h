4f230c7fbb17ce22746bdc2be637614d113a9b2b
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-25 15:31:30 +0100

    Add new NodesWalk class
    This one is more generic.

--- include/dg/analysis/BFS.h
+++ include/dg/analysis/BFS.h
@@ -1,11 +1,30 @@
 #ifndef _DG_BFS_H_
 #define _DG_BFS_H_
 
-#include "dg/analysis/Analysis.h"
 #include "dg/analysis/NodesWalk.h"
 #include "dg/ADT/Queue.h"
 
 using dg::ADT::QueueFIFO;
+
+namespace dg {
+namespace analysis {
+
+template <typename Node,
+          typename VisitTracker = SetVisitTracker<Node>,
+          typename EdgeChooser = SuccessorsEdgeChooser<Node> >
+struct BFS : public NodesWalk<Node, QueueFIFO<Node *>, VisitTracker, EdgeChooser> {
+    BFS() = default;
+    BFS(EdgeChooser chooser) : NodesWalk<Node, QueueFIFO<Node *>, VisitTracker, EdgeChooser>(std::move(chooser)) {}
+    BFS(VisitTracker tracker) : NodesWalk<Node, QueueFIFO<Node *>, VisitTracker, EdgeChooser>(std::move(tracker)) {}
+    BFS(EdgeChooser chooser, VisitTracker tracker)
+    : NodesWalk<Node, QueueFIFO<Node *>, VisitTracker, EdgeChooser>(std::move(chooser), std::move(tracker)) {}
+};
+
+} // namespace analysis
+} // namespace dg
+
+
+#include "dg/analysis/Analysis.h"
 
 namespace dg {
 namespace analysis {
