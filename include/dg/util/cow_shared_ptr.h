ff98d997cc37ee3f93fe5cfa3b8038b0214ad291
Marek Chalupa <xchalup4@fi.muni.cz>
2018-12-11 19:54:00 +0100

    cow_shared_ptr: refactor
    Change the implementation to inherit from shared_ptr

--- include/dg/util/cow_shared_ptr.h
+++ include/dg/util/cow_shared_ptr.h
@@ -1,10 +1,46 @@
 #ifndef _COW_SHARED_PTR_H_
 #define _COW_SHARED_PTR_H_
 
-#include <shared_ptr>
+#include <memory>
 
 ///
 // Shared pointer with copy-on-write support
+template <typename T>
+class cow_shared_ptr : public std::shared_ptr<T> {
+    // am I the owner of the copy?
+    bool owner{true};
+
+    public:
+    cow_shared_ptr() = default;
+    cow_shared_ptr(T *p) : std::shared_ptr<T>(p) {}
+    cow_shared_ptr(cow_shared_ptr&&) = delete;
+    cow_shared_ptr(const cow_shared_ptr& rhs)
+        : std::shared_ptr<T>(rhs), owner(false) {}
+
+    void reset(T *p) {
+        owner = true;
+        std::shared_ptr<T>::reset(p);
+    }
+
+    const T *get() const { return std::shared_ptr<T>::get(); }
+
+    T *getWritable() {
+        if (owner)
+            return std::shared_ptr<T>::get();
+
+        // create a copy of the object and claim the ownership
+        assert(!owner);
+        if (get() != nullptr) {
+            reset(new T(*get()));
+        } else {
+            reset(new T());
+        }
+        assert(owner); // set in reset() method
+        return std::shared_ptr<T>::get();
+    }
+};
+
+/*
 template <typename T>
 class cow_shared_ptr {
     std::shared_ptr<T> ptr{nullptr};
@@ -13,14 +49,14 @@ class cow_shared_ptr {
 
     public:
     cow_shared_ptr() = default;
-    cow_shared_ptr(T *p) : ptr(p) {}
+    cow_shared_ptr(T *p) : ptr(p), owner(true) {}
     cow_shared_ptr(cow_shared_ptr&&) = delete;
     cow_shared_ptr(const cow_shared_ptr& rhs)
         : ptr(rhs.ptr), owner(false) {}
 
     const T *get() const { return ptr.get(); }
 
-    T *getWriteable() {
+    T *getWritable() {
         if (owner)
             return ptr.get();
         // create a copy of the object and claim the ownership
@@ -32,6 +68,11 @@ class cow_shared_ptr {
         owner = true;
         return ptr.get();
     }
+
+    auto use_count() const -> decltype(ptr.use_count()) {
+        return ptr.use_count();
+    }
 };
+*/
 
 #endif  // _COW_SHARED_PTR_H_
