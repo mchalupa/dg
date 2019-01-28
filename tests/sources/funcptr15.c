44ec81af81aa59f7d297d866ae5bd0f481ae6784
Marek Chalupa <xchalup4@fi.muni.cz>
2019-01-28 15:55:35 +0100

    tests: add tests from rda-bug branch

---
+++ tests/sources/funcptr15.c
@@ -0,0 +1,48 @@
+extern void *malloc(unsigned long);
+
+static int mode = 3;
+
+static void *zalloc(unsigned long x) {
+	void *p = malloc(x);
+	memset(p, 0, x);
+	return p;
+}
+
+static void *foo(unsigned long x) {
+	(void)x;
+	static int bar = 0xfeed;
+	return &bar;
+}
+
+static void *foo2(unsigned long x) {
+	(void)x;
+	static int bar = 0xbee;
+	return &bar;
+}
+
+int main(void) {
+	void *(*f)(unsigned long);
+	if (mode == 0)
+		f = malloc;
+	else if (mode == 1)
+		f = zalloc;
+	else if (mode == 2)
+		f = foo;
+	else
+		f = foo2;
+
+	int *p = f(sizeof(int));
+
+	if (mode == 1) {
+		test_assert(*p == 0);
+	} else if (mode == 2) {
+		test_assert(*p == 0xfeed);
+	} else if (mode > 2) {
+		test_assert(*p == 0xbee);
+	}
+
+	if (mode <= 1)
+		free(p);
+
+	return 0;
+}
