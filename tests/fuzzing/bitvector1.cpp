804cf1475c50f3f7fe592cf3270a38b5c9a991db
Marek Chalupa <mchqwerty@gmail.com>
2019-02-07 12:56:24 +0100

    tests: add fuzzing test for sparse bitvector

---
+++ tests/fuzzing/bitvector1.cpp
@@ -0,0 +1,42 @@
+#include <set>
+#include <cassert>
+#include <cstdint>
+
+#include "dg/ADT/Bitvector.h"
+
+using namespace dg::ADT;
+
+extern "C"
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    std::set<uint64_t> S;
+    SparseBitvector B;
+
+    const auto elems = size / (sizeof(uint64_t));
+    const uint64_t *numbers = reinterpret_cast<const uint64_t *>(data);
+    for (unsigned i = 0; i < elems; ++i) {
+        B.set(numbers[i]);
+        S.insert(numbers[i]);
+    }
+
+    for (auto x : S) {
+        assert(B.get(x));
+    }
+
+    for (unsigned int i = 0; i < elems; ++i) {
+        if (S.count(i) > 0)
+            assert(B.get(i));
+        else
+            assert(!B.get(i));
+    }
+
+    for (auto x : S) {
+        assert(B.unset(x));
+    }
+
+    for (auto x : S) {
+        assert(!B.get(x));
+    }
+
+    return 0;
+}
+
