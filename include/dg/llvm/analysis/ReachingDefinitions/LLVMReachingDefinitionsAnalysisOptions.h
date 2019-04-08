4e35673db4f8fed4ebb3226636414bc47638060d
Marek Chalupa <chalupa@fi.muni.cz>
2019-04-08 16:36:15 +0200

    RDA: add models of standard functions
    NOT FINISHED YET

--- include/dg/llvm/analysis/ReachingDefinitions/LLVMReachingDefinitionsAnalysisOptions.h
+++ include/dg/llvm/analysis/ReachingDefinitions/LLVMReachingDefinitionsAnalysisOptions.h
@@ -16,6 +16,38 @@ struct LLVMReachingDefinitionsAnalysisOptions :
     bool threads;
     bool isDense() const { return analysisType == AnalysisType::dense; }
     bool isSparse() const { return analysisType == AnalysisType::ss; }
+
+    LLVMReachingDefinitionsAnalysisOptions() {
+        // setup models for standard functions
+
+        ///
+        // Memory block functions
+        ///
+        // memcpy defines mem. pointed to by operand 0 from the offset 0
+        // to the offset given by the operand 2
+        functionModelAddDef("memcpy", {0, Offset(0), 2});
+        functionModelAddUse("memcpy", {1, Offset(0), 2});
+        functionModelAddDef("llvm.memcpy.p0i8.i64", {0, Offset(0), 1});
+        functionModelAddUse("llvm.memcpy.p0i8.i64", {1, Offset(0), 1});
+
+        functionModelAddDef("memmove", {0, Offset(0), 2});
+        functionModelAddUse("memmove", {1, Offset(0), 2});
+
+        functionModelAddUse("memcmp", {0, Offset(0), 2});
+        functionModelAddUse("memcmp", {1, Offset(0), 2});
+
+        ///
+        // String handling functions
+        ///
+        functionModelAddUse("strlen",  {0, Offset(0), Offset::getUnknown()});
+        functionModelAddUse("strchr",  {0, Offset(0), Offset::getUnknown()});
+        functionModelAddUse("strrchr", {0, Offset(0), Offset::getUnknown()});
+
+        functionModelAddDef("strcpy", {0, Offset(0), Offset::getUnknown()});
+        functionModelAddUse("strcpy", {1, Offset(0), Offset::getUnknown()});
+        functionModelAddDef("strncpy", {0, Offset(0), 2});
+        functionModelAddUse("strncpy", {1, Offset(0), 2});
+    };
 };
 
 } // namespace analysis
