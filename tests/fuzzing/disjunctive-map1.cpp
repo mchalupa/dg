a6e0b8ca5e282e033882515fee8b86071e78636e
Marek Chalupa <mchqwerty@gmail.com>
2019-10-04 13:56:38 +0200

    Move DisjunctiveIntervalMap.h to ADT/
    It's a generic container.

--- tests/fuzzing/disjunctive-map1.cpp
+++ tests/fuzzing/disjunctive-map1.cpp
@@ -6,7 +6,7 @@
 #include <cstdio>
 #endif
 
-#include "dg/analysis/ReachingDefinitions/DisjunctiveIntervalMap.h"
+#include "dg/ADT/DisjunctiveIntervalMap.h"
 
 extern "C"
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
