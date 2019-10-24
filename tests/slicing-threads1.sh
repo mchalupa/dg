51b3bce3838c7390e22a066fa6c30247fd5a4b1f
Marek Chalupa <mchqwerty@gmail.com>
2019-10-24 15:35:52 +0200

    tests: do not run thread1 test with FS PTA
    threads work correctly only with FI

--- tests/slicing-threads1.sh
+++ tests/slicing-threads1.sh
@@ -3,5 +3,9 @@
 TESTS_DIR=`dirname $0`
 source "$TESTS_DIR/test-runner.sh"
 
+if [ "$DG_TESTS_PTA" != "fi" ]; then
+	echo "Unsupported PTA for threads";
+	exit 0
+fi
 export DG_TESTS_SLICER_FLAGS=-threads
 run_test "sources/threads1.c"
