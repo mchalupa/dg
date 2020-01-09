b7dd13927daf69529c2a8a6a15ac946d10e2d6a3
Marek Chalupa <chalupa@fi.muni.cz>
2020-01-09 14:32:24 +0100

    sdg: build paarmeters of functions

--- include/dg/SystemDependenceGraph/DGArgumentPair.h
+++ include/dg/SystemDependenceGraph/DGArgumentPair.h
@@ -12,13 +12,22 @@ class DGParameters;
 class DGArgumentPair : public DGElement {
     friend class DGParameters;
 
-    DGParameters& parameters;
+    DGParameters& _parameters;
 
     DGNodeArgument _input;
     DGNodeArgument _output;
 
     DGArgumentPair(DGParameters& p);
 public:
+
+    DGNodeArgument& getInputArgument() { return _input; }
+    const DGNodeArgument& getInputArgument() const { return _input; }
+
+    DGNodeArgument& getOutputArgument() { return _output; }
+    const DGNodeArgument& getOutputArgument() const { return _output; }
+
+    DGParameters& getParameters() { return _parameters; }
+    const DGParameters& getParameters() const { return _parameters; }
 };
 
 } // namespace sdg
