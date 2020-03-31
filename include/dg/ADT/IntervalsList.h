6a0ed88aaa7062c14c7d9ddd789f132adb1d945f
Marek Chalupa <mchqwerty@gmail.com>
2020-03-31 11:18:27 +0200

    memssa: move intervals list into ADT

---
+++ include/dg/ADT/IntervalsList.h
@@ -0,0 +1,132 @@
+#ifndef DG_INTERVALS_LIST_H_
+#define DG_INTERVALS_LIST_H_
+
+#include <list>
+#include <cassert>
+#include "dg/Offset.h"
+
+namespace dg {
+namespace dda {
+
+class IntervalsList {
+    struct Interval {
+        Offset start;
+        Offset end;
+
+        Interval(Offset s, Offset e) : start(s), end(e) {
+            assert(start <= end);
+        }
+
+        Interval(const std::pair<Offset, Offset>& I) : Interval(I.first, I.second) {}
+
+        bool overlaps(const Interval& I) const {
+            return start <= I.end && end >= I.start;
+        }
+        Offset length() const { return end - start + 1; }
+    };
+
+    std::list<Interval> intervals;
+
+    template <typename Iterator>
+    void _replace_overlapping(const Interval& I, Iterator it, Iterator to) {
+        assert(it->overlaps(I) && to->overlaps(I));
+        assert(it != intervals.end());
+        assert(to != intervals.end());
+        it->start = std::min(it->start, I.start);
+        it->end = std::max(to->end, I.end);
+
+        ++it;
+        ++to;
+        if (it != intervals.end()) {
+                intervals.erase(it, to);
+        } else {
+            assert(to == intervals.end());
+        }
+    }
+
+#ifndef NDEBUG
+    bool _check() {
+        auto it = intervals.begin();
+        if (it != intervals.end()) {
+            assert(it->start < it->end);
+            auto last = it++;
+            while (it != intervals.end()) {
+                assert(last->start < last->end);
+                assert(last->end < it->start);
+                assert(it->start < it->end);
+            }
+        }
+        return true;
+    }
+#endif // NDEBUG
+
+public:
+
+    void add(Offset start, Offset end) {
+        add({start, end});
+    }
+
+    void add(const Interval& I) {
+        if (intervals.empty())
+            intervals.push_back(I);
+
+        auto it = intervals.begin();
+        auto end = intervals.end();
+
+        while (it != end) {
+           if (I.overlaps(*it)) {
+               auto to = ++it;
+               while (to != end && I.overlaps(*to)) {
+                   ++to;
+               }
+               if (to == end) {
+                   --to;
+               }
+               _replace_overlapping(I, it, to);
+               break;
+           } else if (it->start > I.end) {
+               intervals.insert(it, I);
+               break;
+           }
+
+           ++it;
+        }
+
+        if (it == end) {
+            intervals.push_back(I);
+        }
+        assert(_check());
+    }
+
+    IntervalsList& intersectWith(const IntervalsList& rhs) {
+        if (intervals.empty())
+            return *this;
+
+        auto it = intervals.begin();
+        for (auto& RI : rhs.intervals) {
+            while (it->end < RI.start) {
+                auto tmp = it++;
+                intervals.erase(tmp);
+                if (it == intervals.end()) {
+                    return *this;
+                }
+            }
+            if (it->overlaps(RI)) {
+                it->start = std::max(it->start, RI.start);
+                it->end = std::min(it->end, RI.end);
+            }
+        }
+
+        return *this;
+    }
+
+    auto begin() -> decltype (intervals.begin()) { return intervals.begin(); }
+    auto begin() const -> decltype (intervals.begin()) { return intervals.begin(); }
+    auto end() -> decltype (intervals.end()) { return intervals.end(); }
+    auto end()const  -> decltype (intervals.end()) { return intervals.end(); }
+};
+
+} // namespace dda
+} // namespace dg
+
+#endif
--- include/dg/ReadWriteGraph/DefSite.h
+++ include/dg/ReadWriteGraph/DefSite.h
@@ -7,6 +7,7 @@
 #include <list>
 
 #include "dg/Offset.h"
+#include "dg/ADT/IntervalsList.h"
 
 namespace dg {
 namespace dda {
@@ -58,8 +59,7 @@ intervalsOverlap(uint64_t a1, uint64_t a2,
 }
 
 template <typename NodeT>
-struct GenericDefSite
-{
+struct GenericDefSite {
     using NodeTy = NodeT;
 
     GenericDefSite(NodeT *t,
@@ -80,6 +80,24 @@ struct GenericDefSite
         return target == oth.target && offset == oth.offset && len == oth.len;
     }
 
+    Offset end() const {
+        // if the offset is unknown, stretch the interval over all possible bytes
+        if (offset.isUnknown()) {
+            return Offset::UNKNOWN;
+        } else {
+            return offset + (len - 1);
+        }
+    }
+
+    std::pair<Offset, Offset> getInterval() const {
+        // if the offset is unknown, stretch the interval over all possible bytes
+        if (offset.isUnknown()) {
+            return {0, Offset::UNKNOWN};
+        } else {
+            return {offset, offset + (len - 1)};
+        }
+    }
+
     // what memory this node defines
     NodeT *target;
     // on what offset
@@ -93,137 +111,6 @@ using DefSite = GenericDefSite<RWNode>;
 
 extern RWNode *UNKNOWN_MEMORY;
 
-class IntervalsList {
-    struct Interval {
-        Offset start;
-        Offset end;
-
-        Interval(Offset s, Offset e) : start(s), end(e) {
-            assert(start <= end);
-        }
-        Interval(const DefSite& ds) {
-                // if the offset is unknown, stretch the interval over all possible bytes
-                if (ds.offset.isUnknown()) {
-                    start = 0;
-                    end =  Offset::UNKNOWN;
-                } else {
-                    start = ds.offset;
-                    end = ds.offset + (ds.len - 1);
-                }
-
-                assert(start <= end);
-        }
-
-        bool overlaps(const Interval& I) const {
-            return start <= I.end && end >= I.start;
-        }
-        Offset length() const { return end - start + 1; }
-    };
-
-    std::list<Interval> intervals;
-
-    template <typename Iterator>
-    void _replace_overlapping(const Interval& I, Iterator it, Iterator to) {
-        assert(it->overlaps(I) && to->overlaps(I));
-        assert(it != intervals.end());
-        assert(to != intervals.end());
-        it->start = std::min(it->start, I.start);
-        it->end = std::max(to->end, I.end);
-
-        ++it;
-        ++to;
-        if (it != intervals.end()) {
-                intervals.erase(it, to);
-        } else {
-            assert(to == intervals.end());
-        }
-    }
-
-#ifndef NDEBUG
-    bool _check() {
-        auto it = intervals.begin();
-        if (it != intervals.end()) {
-            assert(it->start < it->end);
-            auto last = it++;
-            while (it != intervals.end()) {
-                assert(last->start < last->end);
-                assert(last->end < it->start);
-                assert(it->start < it->end);
-            }
-        }
-        return true;
-    }
-#endif // NDEBUG
-
-public:
-    void add(const DefSite& ds) {
-        add(ds);
-    }
-
-    void add(Offset start, Offset end) {
-        add({start, end});
-    }
-
-    void add(const Interval& I) {
-        if (intervals.empty())
-            intervals.push_back(I);
-
-        auto it = intervals.begin();
-        auto end = intervals.end();
-
-        while (it != end) {
-           if (I.overlaps(*it)) {
-               auto to = ++it;
-               while (to != end && I.overlaps(*to)) {
-                   ++to;
-               }
-               if (to == end) {
-                   --to;
-               }
-               _replace_overlapping(I, it, to);
-               break;
-           } else if (it->start > I.end) {
-               intervals.insert(it, I);
-               break;
-           }
-
-           ++it;
-        }
-
-        if (it == end) {
-            intervals.push_back(I);
-        }
-        assert(_check());
-    }
-
-    IntervalsList& intersectWith(const IntervalsList& rhs) {
-        if (intervals.empty())
-            return *this;
-
-        auto it = intervals.begin();
-        for (auto& RI : rhs.intervals) {
-            while (it->end < RI.start) {
-                auto tmp = it++;
-                intervals.erase(tmp);
-                if (it == intervals.end()) {
-                    return *this;
-                }
-            }
-            if (it->overlaps(RI)) {
-                it->start = std::max(it->start, RI.start);
-                it->end = std::min(it->end, RI.end);
-            }
-        }
-
-        return *this;
-    }
-
-    auto begin() -> decltype (intervals.begin()) { return intervals.begin(); }
-    auto begin() const -> decltype (intervals.begin()) { return intervals.begin(); }
-    auto end() -> decltype (intervals.end()) { return intervals.end(); }
-    auto end()const  -> decltype (intervals.end()) { return intervals.end(); }
-};
-
 // FIXME: change this std::set to std::map (target->offsets)
 class DefSiteSet : public std::set<DefSite> {
 public:
@@ -232,10 +119,10 @@ public:
         std::map<DefSite::NodeTy *, IntervalsList> rhssites;
 
         for (auto& ds : *this) {
-            lhssites[ds.target].add(ds);
+            lhssites[ds.target].add(ds.getInterval());
         }
         for (auto& ds : rhs) {
-            rhssites[ds.target].add(ds);
+            rhssites[ds.target].add(ds.getInterval());
         }
 
         DefSiteSet retval;
