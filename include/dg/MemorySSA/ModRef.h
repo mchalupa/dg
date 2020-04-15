2e392318658a61c8fe0c5c5d7dc022001702a395
Marek Chalupa <mchqwerty@gmail.com>
2020-04-15 07:39:27 +0200

    memssa: factour out modref

---
+++ include/dg/MemorySSA/ModRef.h
@@ -0,0 +1,99 @@
+#ifndef DG_MOD_REF_H_
+#define DG_MOD_REF_H_
+
+#include "dg/Offset.h"
+
+#include "dg/MemorySSA/DefinitionsMap.h"
+#include "dg/ReadWriteGraph/RWNode.h"
+
+
+namespace dg {
+namespace dda {
+
+// sumarized information about visible external
+// effects of the procedure
+class ModRefInfo {
+    // to distinguish between empty and non-computed modref information
+    bool _initialized{false};
+public:
+    // the set of memory that is defined in this procedure
+    // and is external to the subgraph or is local but its address is taken
+    // In other words, memory whose definitions can be "visible"
+    // outside the procedure.
+    // FIXME: we should keep only sets of DefSites
+    DefinitionsMap<RWNode> maydef;
+    // external or local address-taken memory that can be
+    // used inside the procedure
+    // FIXME: we should keep only sets of DefSites
+    DefinitionsMap<RWNode> mayref;
+    // memory that must be defined in this procedure
+    // (on every path through the procedure)
+    DefinitionsMap<RWNode> mustdef;
+
+    void addMayDef(const DefSite& ds, RWNode *def) {
+        // FIXME: do not store def, it is useless. Just takes memory...
+        maydef.add(ds, def);
+    }
+
+    template <typename C>
+    void addMayDef(const C& c, RWNode *def) {
+        for (auto& ds : c) {
+                maydef.add(ds, def);
+        }
+    }
+
+    void addMayRef(const DefSite& ds, RWNode *ref) {
+        // FIXME: do not store ref, it is useless. Just takes memory...
+        mayref.add(ds, ref);
+    }
+
+    template <typename C>
+    void addMayRef(const C& c, RWNode *ref) {
+        for (auto& ds : c) {
+                mayref.add(ds, ref);
+        }
+    }
+
+    void addMustDef(const DefSite& ds, RWNode *def) {
+        // FIXME: do not store def, it is useless. Just takes memory...
+        mustdef.add(ds, def);
+    }
+
+    template <typename C>
+    void addMustDef(const C& c, RWNode *def) {
+        for (auto& ds : c) {
+                mustdef.add(ds, def);
+        }
+    }
+
+    void add(const ModRefInfo& oth) {
+        maydef.add(oth.maydef);
+        mayref.add(oth.mayref);
+        mustdef.add(oth.mustdef);
+    }
+
+    ///
+    // Check whether the procedure may define 'n' (ignoring writes
+    // to unknown memory, \see mayDefineOrUnknown())
+    bool mayDefine(RWNode *n) const { return maydef.definesTarget(n); }
+    bool mayDefineUnknown() const { return mayDefine(UNKNOWN_MEMORY); }
+
+    ///
+    // Check whether the procedure may define 'n', taking into
+    // account also writes to unknown memory
+    bool mayDefineOrUnknown(RWNode *n) const {
+        return mayDefine(n) or mayDefineUnknown();
+    }
+
+    auto getMayDef(RWNode *n) -> decltype(maydef.get(n)) {
+        return maydef.get(n);
+    }
+
+    void setInitialized() { _initialized = true; }
+    bool isInitialized() const { return _initialized  ; }
+}; 
+
+} // namespace dda
+} // namespace dg
+
+#endif
---
+++ lib/MemorySSA/ModRef.cpp
@@ -0,0 +1,76 @@
+#include "dg/MemorySSA/MemorySSA.h"
+#include "dg/util/debug.h"
+
+namespace dg {
+namespace dda {
+
+static inline bool canBeOutput(const RWNode *node, RWSubgraph *subg) {
+    // can escape or already escaped
+    return node->canEscape() ||
+            (!node->getBBlock() || node->getBBlock()->getSubgraph() != subg);
+}
+
+template <typename MR, typename C>
+static void modRefAdd(MR& modref, const C& c, RWNode *node, RWSubgraph *subg) {
+    assert(node && "Node the definion node");
+    for (const DefSite& ds : c) {
+        // can escape
+        if (canBeOutput(ds.target, subg)) {
+            modref.add(ds, node);
+        }
+    }
+}
+
+
+void MemorySSATransformation::computeModRef(RWSubgraph *subg, SubgraphInfo& si) {
+
+    if (si.modref.isInitialized()) {
+        return;
+    }
+
+    DBG_SECTION_BEGIN(dda, "Computing modref for subgraph " << subg->getName());
+
+    // set it here due to recursive procedures
+    si.modref.setInitialized();
+
+    // iterate over the blocks (note: not over the infos, those
+    // may not be created if the block was not used yet
+    for (auto *b : subg->bblocks()) {
+        auto& bi = si.getBBlockInfo(b);
+        if (bi.isCallBlock()) {
+            auto *C = bi.getCall();
+            for (auto& callee : C->getCallees()) {
+                auto *csubg = callee.getSubgraph();
+                if (csubg) {
+                    auto& callsi = getSubgraphInfo(csubg);
+                    computeModRef(csubg, callsi);
+                    assert(callsi.modref.isInitialized());
+
+                    si.modref.add(callsi.modref);
+                } else {
+                    // undefined function
+                    modRefAdd(si.modref.maydef,
+                              callee.getCalledValue()->getDefines(),
+                              C, csubg);
+                    modRefAdd(si.modref.maydef,
+                              callee.getCalledValue()->getOverwrites(),
+                              C, csubg);
+                    modRefAdd(si.modref.mayref,
+                              callee.getCalledValue()->getUses(),
+                              C, csubg);
+                }
+            }
+        } else {
+            // do not perform LVN if not needed, just scan the nodes
+            for (auto *node : b->getNodes()) {
+                modRefAdd(si.modref.maydef, node->getDefines(), node, subg);
+                modRefAdd(si.modref.maydef, node->getOverwrites(), node, subg);
+                modRefAdd(si.modref.mayref, node->getUses(), node, subg);
+            }
+        }
+    }
+    DBG_SECTION_END(dda, "Computing modref for subgraph " << subg->getName() << " done");
+}
+
+} // namespace dda
+} // namespace dg
