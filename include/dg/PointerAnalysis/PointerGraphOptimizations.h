28b666242f6e8140dc116190ccb29eee5aa96f68
Marek Chalupa <mchqwerty@gmail.com>
2020-11-10 20:33:07 +0100

    PTA: move more methods to .cpp file

--- include/dg/PointerAnalysis/PointerGraphOptimizations.h
+++ include/dg/PointerAnalysis/PointerGraphOptimizations.h
@@ -28,23 +28,6 @@ public:
     };
 };
 
-static inline bool isStoreOfUnknown(PSNode *S, PSNode *to) {
-    return (S->getType() == PSNodeType::STORE &&
-            S->getOperand(1) == to &&
-            S->getOperand(0)->isUnknownMemory());
-}
-
-static inline bool usersImplyUnknown(PSNode *nd) {
-    for (PSNode *user : nd->getUsers()) {
-        // we store only unknown to this memory
-        if (!isStoreOfUnknown(user, nd) &&
-            user->getType() != PSNodeType::LOAD)
-            return false;
-    }
-
-    return true;
-}
-
 static inline bool allOperandsAreSame(PSNode *nd) {
     auto opNum = nd->getOperandsNum();
     if (opNum < 1)
@@ -70,51 +53,7 @@ class PSUnknownsReducer {
 
     unsigned removed = 0;
 
-    void processAllocs() {
-        for (const auto& nd : PS->getNodes()) {
-            if (!nd)
-                continue;
-
-            if (nd->getType() == PSNodeType::ALLOC) {
-                // this is an allocation that has only stores of unknown memory to it
-                // (and its address is not stored anywhere) and there are only loads
-                // from this memory (that must result to unknown)
-                if (usersImplyUnknown(nd.get())) {
-                    for (PSNode *user : nd->getUsers()) {
-                        if (user->getType() == PSNodeType::LOAD) {
-                            // replace the uses of the load value by unknown
-                            // (this is what would happen in the analysis)
-                            user->replaceAllUsesWith(UNKNOWN_MEMORY);
-                            mapping.add(user, UNKNOWN_MEMORY);
-                        }
-                        // store can be removed directly
-                        user->isolate();
-                        PS->remove(user);
-                        ++removed;
-                    }
-
-                    // NOTE: keep the alloca, as it contains the
-                    // pointer to itself and may be queried for this pointer
-                }
-            } else if (nd->getType() == PSNodeType::PHI && nd->getOperandsNum() == 0) {
-                for (PSNode *user : nd->getUsers()) {
-                    // replace the uses of this value with unknown
-                    user->replaceAllUsesWith(UNKNOWN_MEMORY);
-                    mapping.add(user, UNKNOWN_MEMORY);
-
-                    // store can be removed directly
-                    user->isolate();
-                    PS->remove(user);
-                    ++removed;
-                }
-
-                nd->isolate();
-                PS->remove(nd.get());
-                assert(nd.get() == nullptr);
-                ++removed;
-            }
-        }
-    }
+    void processAllocs();
 
 public:
     PSUnknownsReducer(PointerGraph *PS) : PS(PS) {}
