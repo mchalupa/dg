a9cf7d6717af464acecde839f650a5fba44d7a57
Marek Chalupa <mchqwerty@gmail.com>
2020-11-11 16:47:02 +0100

    tests: add tests for hash maps

--- tests/adt-test.cpp
+++ tests/adt-test.cpp
@@ -105,3 +105,98 @@ TEST_CASE("Intervals handling", "RWG intervals") {
     REQUIRE(!intervalsOverlap(1, 2, 3, 3));
 }
 
+#include "dg/ADT/STLHashMap.h"
+
+template <typename MapT>
+void hashMapTest() {
+    MapT M;
+    REQUIRE(M.get(0) == nullptr);
+    auto r = M.put(1, 2);
+    const int *x = M.get(1);
+    REQUIRE(r);
+    REQUIRE(x);
+    REQUIRE(*x == 2);
+    r = M.put(1, 3);
+    REQUIRE(!r);
+    M.put(5, 6);
+    x = M.get(1);
+    const int *y = M.get(5);
+    REQUIRE(x);
+    REQUIRE(*x == 2);
+    REQUIRE(y);
+    REQUIRE(*y == 6);
+    M.erase(1);
+    REQUIRE(M.get(1) == nullptr);
+    y = M.get(5);
+    REQUIRE(y);
+    REQUIRE(*y == 6);
+}
+
+
+// create an object for testing hashing function collision
+struct MyInt {
+    int x;
+    MyInt() : x(0) {} // google sparsehash needs default ctor
+    MyInt(int y) : x(y) {}
+    bool operator==(const MyInt& rhs) const { return x == rhs.x; }
+};
+
+namespace std {
+template <> struct hash<MyInt> {
+    size_t operator()(const MyInt& mi) const {
+        return mi.x % 2;
+    }
+};
+}
+
+template <typename MapT>
+void hashCollisionTest() {
+    MapT M;
+    bool r;
+    r = M.put(MyInt(2), 2);
+    REQUIRE(r);
+    r = M.put(MyInt(3), 3);
+    REQUIRE(r);
+    // collision
+    r = M.put(MyInt(4), 4);
+    REQUIRE(r);
+    REQUIRE(M.size() == 3);
+    for (int i = 2; i <= 4; ++i) {
+        const int *x = M.get(MyInt(i));
+        REQUIRE(*x == i);
+    }
+}
+
+TEST_CASE("STL hashmap test", "HashMap") {
+    hashMapTest<dg::STLHashMap<int, int>>();
+}
+
+TEST_CASE("STL hashmap collision test", "HashMap") {
+    hashCollisionTest<dg::STLHashMap<MyInt, int>>();
+}
+
+#ifdef HAVE_GOOGLE_SPARSEHASH
+#include "dg/ADT/GoogleHashMap.h"
+
+TEST_CASE("Google sparsehash test", "HashMap") {
+    hashMapTest<dg::SparseHashMap<int, int>>();
+}
+
+TEST_CASE("Google sparehash collision test", "HashMap") {
+    hashCollisionTest<dg::SparseHashMap<MyInt, int>>();
+}
+#endif
+
+#ifdef HAVE_TSL_HOPSCOTCH
+#include "dg/ADT/TslHopscotchHashMap.h"
+
+TEST_CASE("TSL Hopscotch hashmap test", "HashMap") {
+    hashMapTest<dg::HopscotchHashMap<int, int>>();
+}
+
+TEST_CASE("TSL Hopscotch collision test", "HashMap") {
+    hashCollisionTest<dg::HopscotchHashMap<MyInt, int>>();
+}
+#endif
+
+
