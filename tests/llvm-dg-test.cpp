209ccebce34c953089f55d0f2a563b0cd0550816
Marek Chalupa <mchqwerty@gmail.com>
2020-11-11 15:29:24 +0100

    tests: rewrite llvm-dg-test to Catch

--- tests/llvm-dg-test.cpp
+++ tests/llvm-dg-test.cpp
@@ -1,70 +1,47 @@
-#include <assert.h>
-#include <cstdarg>
-#include <cstdio>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 #include "dg/llvm/LLVMDependenceGraph.h"
 #include "dg/DFS.h"
-#include "test-runner.h"
 
+TEST_CASE("reference counting test", "LLVM DG") {
+    using namespace dg;
 
-namespace dg {
-namespace tests {
+    LLVMDependenceGraph d;
+    LLVMDependenceGraph s;
 
-struct TestRefcount : public Test
-{
-    TestRefcount() : Test("reference counting test") {}
+    int rc;
+    rc = s.ref();
+    REQUIRE(rc == 2);
+    rc = s.unref();
+    REQUIRE(rc == 1);
 
-    void test()
-    {
-        LLVMDependenceGraph d;
-        LLVMDependenceGraph s;
+    s.ref();
+    rc = s.ref();
+    REQUIRE(rc == 3);
+    s.unref();
+    rc = s.unref();
+    REQUIRE(rc == 1);
 
-        int rc;
-        rc = s.ref();
-        check(rc == 2, "refcount shold be 2, but is %d", rc);
-        rc = s.unref();
-        check(rc == 1, "refcount shold be 1, but is %d", rc);
+    // addSubgraph increases refcount
+    LLVMNode n1(nullptr), n2(nullptr);
+    n1.addSubgraph(&s);
+    n2.addSubgraph(&s);
 
-        s.ref();
-        rc = s.ref();
-        check(rc == 3, "refcount shold be 3, but is %d", rc);
-        s.unref();
-        rc = s.unref();
-        check(rc == 1, "refcount shold be 1, but is %d", rc);
+    // we do not have a getter for refcounts, so just
+    // inc and dec the counter to get the current value
+    s.ref();
+    rc = s.unref();
+    REQUIRE(rc == 3);
 
-        // addSubgraph increases refcount
-        LLVMNode n1(nullptr), n2(nullptr);
-        n1.addSubgraph(&s);
-        n2.addSubgraph(&s);
+    // set entry blocks, otherwise the constructor will call assert
+    LLVMBBlock *entryBB1 = new LLVMBBlock(&n1);
+    LLVMBBlock *entryBB2 = new LLVMBBlock(&n2);
 
-        // we do not have a getter for refcounts, so just
-        // inc and dec the counter to get the current value
-        s.ref();
-        rc = s.unref();
-        check(rc == 3, "refcount shold be 3, but is %d", rc);
+    d.setEntryBB(entryBB1);
+    s.setEntryBB(entryBB2);
 
-        // set entry blocks, otherwise the constructor will call assert
-        LLVMBBlock *entryBB1 = new LLVMBBlock(&n1);
-        LLVMBBlock *entryBB2 = new LLVMBBlock(&n2);
-
-        d.setEntryBB(entryBB1);
-        s.setEntryBB(entryBB2);
-
-        delete entryBB1;
-        delete entryBB2;
-    }
-};
-
-}
+    delete entryBB1;
+    delete entryBB2;
 }
 
-int main(void)
-{
-    using namespace dg::tests;
-
-    TestRunner Runner;
-
-    Runner.add(new TestRefcount());
-
-    return Runner();
-}
