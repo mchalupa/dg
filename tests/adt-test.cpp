caa595b2bfda3acbfc3981094d4e4962ffb73cd7
Marek Chalupa <mchqwerty@gmail.com>
2020-11-11 15:25:53 +0100

    tests: rewrite adt-test to Catch
    We need to get of our custom runner finally!

--- tests/adt-test.cpp
+++ tests/adt-test.cpp
@@ -1,8 +1,5 @@
-#include <assert.h>
-#include <cstdarg>
-#include <cstdio>
-
-#include "test-runner.h"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 #include "dg/ADT/Queue.h"
 #include "dg/ADT/Bitvector.h"
@@ -11,157 +8,100 @@
 using namespace dg::ADT;
 using dg::Offset;
 
-namespace dg {
-namespace tests {
+TEST_CASE("QuieieLIFO basic manimp", "QueueLIFO") {
+    QueueLIFO<int> queue;
+    REQUIRE(queue.empty());
 
-class TestLIFO : public Test
-{
-public:
-    TestLIFO() : Test("LIFO test")
-    {}
+    queue.push(1);
+    queue.push(13);
+    queue.push(4);
+    queue.push(2);
+    queue.push(2);
 
-    void test()
-    {
-        QueueLIFO<int> queue;
-        check(queue.empty(), "empty queue not empty");
-
-        queue.push(1);
-        queue.push(13);
-        queue.push(4);
-        queue.push(2);
-        queue.push(2);
-
-        check(queue.pop() == 2, "Wrong pop order");
-        check(queue.pop() == 2, "Wrong pop order");
-        check(queue.pop() == 4, "Wrong pop order");
-        check(queue.pop() == 13, "Wrong pop order");
-        check(queue.pop() == 1, "Wrong pop order");
-        check(queue.empty(), "emptied queue not empty");
-    }
-};
-
-class TestFIFO : public Test
-{
-public:
-    TestFIFO() : Test("FIFO test")
-    {}
-
-    void test()
-    {
-        QueueFIFO<int> queue;
-        check(queue.empty(), "empty queue not empty");
-
-        queue.push(1);
-        queue.push(13);
-        queue.push(4);
-        queue.push(4);
-        queue.push(2);
-
-        check(queue.pop() == 1, "Wrong pop order");
-        check(queue.pop() == 13, "Wrong pop order");
-        check(queue.pop() == 4, "Wrong pop order");
-        check(queue.pop() == 4, "Wrong pop order");
-        check(queue.pop() == 2, "Wrong pop order");
-        check(queue.empty(), "emptied queue not empty");
-    }
-};
-
-struct mycomp
-{
-    bool operator()(int a, int b) const
-    {
-        return a > b;
-    }
-};
-
-class TestPrioritySet : public Test
-{
-public:
-    TestPrioritySet() : Test("test priority set")
-    {}
-
-    void test()
-    {
-        PrioritySet<int, mycomp> queue;
-        check(queue.empty(), "empty queue not empty");
-
-        queue.push(1);
-        queue.push(13);
-        queue.push(4);
-        queue.push(4);
-        queue.push(2);
-
-        // we inserted twice, but it is a set
-        check(queue.size() == 4, "BUG in size");
-
-        check(queue.pop() == 13, "Wrong pop order");
-        check(queue.pop() == 4, "Wrong pop order");
-        check(queue.pop() == 2, "Wrong pop order");
-        check(queue.pop() == 1, "Wrong pop order");
-        check(queue.empty(), "emptied queue not empty");
-    }
-};
-
-class TestIntervalsHandling : public Test
-{
-public:
-    TestIntervalsHandling() : Test("test intervals handling")
-    {}
-
-    void test()
-    {
-        using namespace dda;
-
-        check(intervalsDisjunctive(0, 1, 2, 20), "BUG: intervals should be disjunctive");
-        check(intervalsDisjunctive(0, 1, 1, 2), "BUG: intervals should be disjunctive");
-        check(!intervalsDisjunctive(1, 1, 1, 2), "BUG: intervals should not be disjunctive");
-        check(!intervalsDisjunctive(1, 1, 1, 1), "BUG: intervals should not be disjunctive");
-        check(!intervalsDisjunctive(3, 5, 3, 5), "BUG: intervals should not be disjunctive");
-        check(!intervalsDisjunctive(3, 7, 3, 5), "BUG: intervals should not be disjunctive");
-        check(!intervalsDisjunctive(3, 5, 3, 7), "BUG: intervals should not be disjunctive");
-        check(intervalsDisjunctive(1, 1, 2, 2), "BUG: intervals should be disjunctive");
-        check(!intervalsDisjunctive(0, 4, 2, 2), "BUG: intervals should not be disjunctive");
-
-        check(!intervalsDisjunctive(0, 4, 2, Offset::UNKNOWN),
-                                    "BUG: intervals should not be disjunctive");
-        check(intervalsDisjunctive(0, 4, 4, Offset::UNKNOWN),
-                                   "BUG: intervals should be disjunctive");
-        check(!intervalsDisjunctive(0, Offset::UNKNOWN, 4, Offset::UNKNOWN),
-                                    "BUG: intervals should not be disjunctive");
-        check(!intervalsDisjunctive(0, Offset::UNKNOWN, 1, 4),
-                                    "BUG: intervals should not be disjunctive");
-
-        check(!intervalsOverlap(0, 1, 2, 20), "BUG: intervals should be disjunctive");
-        check(!intervalsOverlap(0, 1, 1, 2), "BUG: intervals should not overlap");
-        check(intervalsOverlap(1, 1, 1, 2), "BUG: intervals should overlap");
-        check(intervalsOverlap(1, 1, 1, 1), "BUG: intervals should overlap");
-        check(intervalsOverlap(3, 5, 3, 5), "BUG: intervals should overlap");
-        check(intervalsOverlap(3, 7, 3, 5), "BUG: intervals should overlap");
-        check(intervalsOverlap(3, 5, 3, 7), "BUG: intervals should overlap");
-        check(!intervalsOverlap(1, 1, 2, 2), "BUG: intervals should be disjunctive");
-        check(!intervalsOverlap(1, 2, 0, 1), "BUG: intervals should not overlap");
-        check(intervalsOverlap(1, 2, 1, 1), "BUG: intervals should overlap");
-        check(intervalsOverlap(1, 2, 1, 2), "BUG: intervals should overlap");
-        check(intervalsOverlap(1, 2, 2, 2), "BUG: intervals should overlap");
-        check(intervalsOverlap(2, 2, 2, 2), "BUG: intervals should overlap");
-        check(intervalsOverlap(3, 3, 2, 2), "BUG: intervals should overlap");
-        check(!intervalsOverlap(1, 2, 3, 3), "BUG: intervals should not overlap");
-        check(!intervalsOverlap(1, 2, 3, 3), "BUG: intervals should not overlap");
-    }
-};
-
-}; // namespace tests
-}; // namespace dg
-
-int main()
-{
-    using namespace dg::tests;
-    TestRunner Runner;
-
-    Runner.add(new TestLIFO());
-    Runner.add(new TestFIFO());
-    Runner.add(new TestPrioritySet());
-    Runner.add(new TestIntervalsHandling());
-
-    return Runner();
+    REQUIRE(queue.pop() == 2);
+    REQUIRE(queue.pop() == 2);
+    REQUIRE(queue.pop() == 4);
+    REQUIRE(queue.pop() == 13);
+    REQUIRE(queue.pop() == 1);
+    REQUIRE(queue.empty());
 }
+
+TEST_CASE("QueueFIFo basic manimp", "QueueFIFO") {
+    QueueFIFO<int> queue;
+    REQUIRE(queue.empty());
+
+    queue.push(1);
+    queue.push(13);
+    queue.push(4);
+    queue.push(4);
+    queue.push(2);
+
+    REQUIRE(queue.pop() == 1);
+    REQUIRE(queue.pop() == 13);
+    REQUIRE(queue.pop() == 4);
+    REQUIRE(queue.pop() == 4);
+    REQUIRE(queue.pop() == 2);
+    REQUIRE(queue.empty());
+}
+
+struct mycomp {
+    bool operator()(int a, int b) const { return a > b; }
+};
+
+TEST_CASE("Priority queue basic manimp", "Priority Queue") 
+{
+    PrioritySet<int, mycomp> queue;
+    REQUIRE(queue.empty());
+
+    queue.push(1);
+    queue.push(13);
+    queue.push(4);
+    queue.push(4);
+    queue.push(2);
+
+    // we inserted twice, but it is a set
+    REQUIRE(queue.size() == 4);
+
+    REQUIRE(queue.pop() == 13);
+    REQUIRE(queue.pop() == 4);
+    REQUIRE(queue.pop() == 2);
+    REQUIRE(queue.pop() == 1);
+    REQUIRE(queue.empty());
+}
+
+TEST_CASE("Intervals handling", "RWG intervals") {
+    using namespace dg::dda;
+
+    REQUIRE(intervalsDisjunctive(0, 1, 2, 20));
+    REQUIRE(intervalsDisjunctive(0, 1, 1, 2));
+    REQUIRE(!intervalsDisjunctive(1, 1, 1, 2));
+    REQUIRE(!intervalsDisjunctive(1, 1, 1, 1));
+    REQUIRE(!intervalsDisjunctive(3, 5, 3, 5));
+    REQUIRE(!intervalsDisjunctive(3, 7, 3, 5));
+    REQUIRE(!intervalsDisjunctive(3, 5, 3, 7));
+    REQUIRE(intervalsDisjunctive(1, 1, 2, 2));
+    REQUIRE(!intervalsDisjunctive(0, 4, 2, 2));
+
+    REQUIRE(!intervalsDisjunctive(0, 4, 2, Offset::UNKNOWN));
+    REQUIRE(intervalsDisjunctive(0, 4, 4, Offset::UNKNOWN));
+    REQUIRE(!intervalsDisjunctive(0, Offset::UNKNOWN, 4, Offset::UNKNOWN));
+    REQUIRE(!intervalsDisjunctive(0, Offset::UNKNOWN, 1, 4));
+
+    REQUIRE(!intervalsOverlap(0, 1, 2, 20));
+    REQUIRE(!intervalsOverlap(0, 1, 1, 2));
+    REQUIRE(intervalsOverlap(1, 1, 1, 2));
+    REQUIRE(intervalsOverlap(1, 1, 1, 1));
+    REQUIRE(intervalsOverlap(3, 5, 3, 5));
+    REQUIRE(intervalsOverlap(3, 7, 3, 5));
+    REQUIRE(intervalsOverlap(3, 5, 3, 7));
+    REQUIRE(!intervalsOverlap(1, 1, 2, 2));
+    REQUIRE(!intervalsOverlap(1, 2, 0, 1));
+    REQUIRE(intervalsOverlap(1, 2, 1, 1));
+    REQUIRE(intervalsOverlap(1, 2, 1, 2));
+    REQUIRE(intervalsOverlap(1, 2, 2, 2));
+    REQUIRE(intervalsOverlap(2, 2, 2, 2));
+    REQUIRE(intervalsOverlap(3, 3, 2, 2));
+    REQUIRE(!intervalsOverlap(1, 2, 3, 3));
+    REQUIRE(!intervalsOverlap(1, 2, 3, 3));
+}
+
