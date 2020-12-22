f02a25caca98a57e1844c353f3456d202ef6d845
Lukas Zaoral <lzaoral@redhat.com>
2020-12-22 18:11:01 +0100

    tests: Add cmd-args test
    Test that all binaries are functional.

---
+++ tests/cmd-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from os import listdir
+from subprocess import DEVNULL, PIPE, Popen
+
+failed = False
+
+for f in filter(lambda x: x.startswith('llvm'), listdir()):
+    print(f, end='')
+    p = Popen(['./' + f, '--version'], stdout=DEVNULL, stderr=PIPE)
+    out = p.communicate()[1].decode()
+
+    if p.returncode == 0:
+        print("\u001b[32m OK\u001b[0m")
+        continue
+
+    failed = True
+    print("\u001b[31m NOK\u001b[0m")
+    print(out)
+
+exit(failed)
