17f6fcf7be8cae03f53ba0d241e81f0c8ea7bda2
Lukas Zaoral <lzaoral@redhat.com>
2021-03-09 11:04:04 +0100

    fuzzing-tests: Simplify code

--- tests/fuzzing/numbers-set1.cpp
+++ tests/fuzzing/numbers-set1.cpp
@@ -1,28 +1,25 @@
 #undef NDEBUG
 
-#include <set>
 #include <cassert>
 #include <cstdint>
+#include <set>
 
 #include "dg/ADT/NumberSet.h"
-
-using namespace dg::ADT;
 
 extern "C"
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     std::set<uint64_t> S;
-    BitvectorNumberSet B;
+    dg::ADT::BitvectorNumberSet B;
 
-    const auto elems = size / (sizeof(uint64_t));
+    const auto elems = size / sizeof(uint64_t);
     const uint64_t *numbers = reinterpret_cast<const uint64_t *>(data);
     for (unsigned i = 0; i < elems; ++i) {
         B.add(numbers[i]);
         S.insert(numbers[i]);
     }
 
-    for (auto x : S) {
+    for (auto x : S)
         assert(B.has(x));
-    }
 
     for (unsigned int i = 0; i < elems; ++i) {
         if (S.count(i) > 0)
@@ -33,4 +30,3 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
     return 0;
 }
-
