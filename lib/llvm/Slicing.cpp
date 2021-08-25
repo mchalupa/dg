5283c8143110ed53195b403ac5d68091e39893fd
Marek Chalupa <mchqwerty@gmail.com>
2021-08-25 18:17:33 +0200

    savior

--- lib/llvm/Slicing.cpp
+++ lib/llvm/Slicing.cpp
@@ -3,6 +3,7 @@
 
 #include "dg/llvm/LLVMFastSlicer.h"
 #include "dg/ADT/Queue.h"
+#include "dg/ADT/HashMap.h"
 
 
 namespace dg {
@@ -15,9 +16,9 @@ struct Data {
     std::set<llvm::Value *> search_defs{};
 };
 
-class SlicerWorker {
+class SlicerImpl {
     std::set<llvm::Value *> slice;
-    ADT::HashMap<llvm::Instruction *, Data> data;
+    dg::HashMap<llvm::Instruction *, Data> data;
     ADT::QueueFIFO<llvm::Instruction *> queue;
 
     bool addTopLevelValuesToSlice(llvm::Value *val) {
@@ -34,30 +35,40 @@ class SlicerWorker {
             auto *curI = llvm::dyn_cast<llvm::Instruction>(cur);
             if (!curI)
                 continue;
-            for (auto *op : curI->operands()) {
-                tmpqueue.push(op);
+            for (auto &op : curI->operands()) {
+                tmpqueue.push(op.get());
             }
         }
         return changed;
+    }
+
+public:
+    std::set<llvm::Value *>
+    computeSlice(const std::vector<const llvm::Value *> &criteria) {
+        for (auto *c : criteria) {
+            addTopLevelValuesToSlice(const_cast<llvm::Value*>(c));
+        }
+
+        /*
+        changed = false;
+        while (!queue.empty()) {
+            auto *I = queue.pop();
+        }
+        */
+
+        return slice;
     }
 };
 
 std::set<llvm::Value *>
 LLVMFastSlicer::computeSlice(const std::vector<const llvm::Value *> &criteria) {
-    SlicerWorker slicer;
-    for (auto *c : criteria) {
-        slicer.addTopLevelValuesToSlice(c);
-    }
-
-    changed = false;
-    while (!queue.empty()) {
-        auto *I = queue.pop();
-    }
-
-    return slice;
+    return SlicerImpl().computeSlice(criteria);
 }
 
 void LLVMFastSlicer::sliceModule(const std::set<llvm::Value *> &slice) {
+    for (auto *val : slice) {
+        llvm::errs() << "IN SLICE: " << *val << "\n";
+    }
 }
 
 } // namespace llvmdg
