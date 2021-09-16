b5418eb396948d1ee22e99edf589e42ef2a8ff38
Marek Chalupa <mchqwerty@gmail.com>
2021-09-16 15:21:31 +0200

    CDA: Swap NTSCD and NTSCD2 algorithms
    NTSCD2 is the one that remembers frontiers.
    It is more efficient in practice, so use it.

--- lib/ControlDependence/NTSCD.h
+++ lib/ControlDependence/NTSCD.h
@@ -16,77 +16,6 @@ class NTSCD {
     using ResultT = std::map<CDNode *, std::set<CDNode *>>;
 
     struct Info {
-        unsigned colored{false};
-        unsigned short counter;
-    };
-
-    std::unordered_map<CDNode *, Info> data;
-
-    void compute(CDGraph &graph, CDNode *target) {
-        // initialize nodes
-        for (auto *nd : graph) {
-            auto &D = data[nd];
-            D.colored = false;
-            D.counter = nd->successors().size();
-        }
-
-        // initialize the search
-        data[target].colored = true;
-        ADT::QueueLIFO<CDNode *> queue;
-        queue.push(target);
-
-        // search!
-        while (!queue.empty()) {
-            auto *node = queue.pop();
-            assert(data[node].colored && "A non-colored node in queue");
-
-            for (auto *pred : node->predecessors()) {
-                auto &D = data[pred];
-                --D.counter;
-                if (D.counter == 0) {
-                    D.colored = true;
-                    queue.push(pred);
-                }
-            }
-        }
-    }
-
-  public:
-    // returns control dependencies and reverse control dependencies
-    std::pair<ResultT, ResultT> compute(CDGraph &graph) {
-        ResultT CD;
-        ResultT revCD;
-
-        data.reserve(graph.size());
-
-        for (auto *nd : graph) {
-            compute(graph, nd);
-
-            for (auto *predicate : graph.predicates()) {
-                bool has_colored = false;
-                bool has_uncolored = false;
-                for (auto *succ : predicate->successors()) {
-                    if (data[succ].colored)
-                        has_colored = true;
-                    if (!data[succ].colored)
-                        has_uncolored = true;
-                }
-
-                if (has_colored && has_uncolored) {
-                    CD[nd].insert(predicate);
-                    revCD[predicate].insert(nd);
-                }
-            }
-        }
-
-        return {CD, revCD};
-    }
-};
-
-class NTSCD2 {
-    using ResultT = std::map<CDNode *, std::set<CDNode *>>;
-
-    struct Info {
         unsigned color{0};
     };
 
@@ -165,6 +94,77 @@ class NTSCD2 {
 
         for (auto *nd : graph) {
             compute(graph, nd, CD, revCD);
+        }
+
+        return {CD, revCD};
+    }
+};
+
+class NTSCD2 {
+    using ResultT = std::map<CDNode *, std::set<CDNode *>>;
+
+    struct Info {
+        unsigned colored{false};
+        unsigned short counter;
+    };
+
+    std::unordered_map<CDNode *, Info> data;
+
+    void compute(CDGraph &graph, CDNode *target) {
+        // initialize nodes
+        for (auto *nd : graph) {
+            auto &D = data[nd];
+            D.colored = false;
+            D.counter = nd->successors().size();
+        }
+
+        // initialize the search
+        data[target].colored = true;
+        ADT::QueueLIFO<CDNode *> queue;
+        queue.push(target);
+
+        // search!
+        while (!queue.empty()) {
+            auto *node = queue.pop();
+            assert(data[node].colored && "A non-colored node in queue");
+
+            for (auto *pred : node->predecessors()) {
+                auto &D = data[pred];
+                --D.counter;
+                if (D.counter == 0) {
+                    D.colored = true;
+                    queue.push(pred);
+                }
+            }
+        }
+    }
+
+  public:
+    // returns control dependencies and reverse control dependencies
+    std::pair<ResultT, ResultT> compute(CDGraph &graph) {
+        ResultT CD;
+        ResultT revCD;
+
+        data.reserve(graph.size());
+
+        for (auto *nd : graph) {
+            compute(graph, nd);
+
+            for (auto *predicate : graph.predicates()) {
+                bool has_colored = false;
+                bool has_uncolored = false;
+                for (auto *succ : predicate->successors()) {
+                    if (data[succ].colored)
+                        has_colored = true;
+                    if (!data[succ].colored)
+                        has_uncolored = true;
+                }
+
+                if (has_colored && has_uncolored) {
+                    CD[nd].insert(predicate);
+                    revCD[predicate].insert(nd);
+                }
+            }
         }
 
         return {CD, revCD};
