7aab933a4234f8147426b051e5f8d252c64a4c4e
Lukas Zaoral <lzaoral@redhat.com>
2021-10-23 17:11:05 +0200

    tests/slicing: fix unaligned accesses in tests
    According to C11, unaligned access is UB:
    
    A pointer to an object type may be converted to a pointer to a different
    object type.  If the resulting pointer is not correctly aligned for the
    referenced type, the behavior is undefined.

--- tests/slicing/sources/pointers6.c
+++ tests/slicing/sources/pointers6.c
@@ -1,12 +1,14 @@
+#include <stdalign.h>
+
 int main(void) {
     int b = 4;
-    char array[100];
+    alignas(alignof(int **)) char array[100];
 
-    int **p = (int **) (array + 7);
+    int **p = (int **) (array + 7 * alignof(int **));
     *p = &b;
 
     p = 0;
-    int *q = *((int **) (array + 7));
+    int *q = *((int **) (array + 7 * alignof(int **)));
     *q = 3;
 
     test_assert(b == 3);
