4555ab72a1d50b52889412b2263637aa5519722f
Lukas Zaoral <lzaoral@redhat.com>
2021-10-27 15:40:13 +0200

    tests/slicing/sources/bitcast: relax conditions in static assertions

--- tests/slicing/sources/bitcast5.c
+++ tests/slicing/sources/bitcast5.c
@@ -1,9 +1,13 @@
 #include <stdalign.h>
 
-_Static_assert(sizeof(int) == 4, "This test assumes sizeof(int) == 4");
-
 int main(void) {
     alignas(alignof(int)) char a[] = "Hello, world";
+
+    _Static_assert(2 * sizeof(int) <= sizeof a &&
+                           sizeof(int) > 3 * sizeof(char),
+                   "This test assumes that 2 * sizeof(int) <= sizeof a && "
+                   "sizeof(int) > 3 * sizeof(char)");
+
     int *p = (int *) a + 1;
     *p = 0;
 
