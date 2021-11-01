e4a991fbc128ea32350e63b1d94d20091906822b
Lukas Zaoral <lzaoral@redhat.com>
2021-11-01 17:41:48 +0100

    tests/slicing/sources/bitcast{2,3}: use unsigned char to store 0xab
    to silence the following clang-tidy warning:
    
    Narrowing conversion from constant value 171 (0x000000AB) of type 'int' to
    signed type 'char' is implementation-defined

--- tests/slicing/sources/bitcast3.c
+++ tests/slicing/sources/bitcast3.c
@@ -2,13 +2,12 @@ _Static_assert(sizeof(int) == 4, "This test assumes sizeof(int) == 4");
 
 union BYTE {
     int i;
-    char b[4];
+    unsigned char b[4];
 };
 
 /* test accessing bytes in int */
 int main(void) {
-    union BYTE B;
-    B.i = 0;
+    union BYTE B = {.i = 0};
     B.b[0] = 0xab;
     B.b[1] = 0xab;
     B.b[2] = 0xab;
