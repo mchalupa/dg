97c978567829134dca799382e1f9688adb41b825
Anna Rechtackova <anna.rechty@gmail.com>
2021-11-14 23:06:40 +0100

    VR: more tests and compilation flags

--- tests/value-relations-test.cpp
+++ tests/value-relations-test.cpp
@@ -24,6 +24,39 @@ CollectedEdges collect(RelationsGraph::iterator begin,
     return result;
 }
 
+void checkSize(const CollectedEdges &result, const RelationsGraph &graph,
+               size_t expectedSize) {
+    INFO("result " << dump(result));
+    INFO("graph:\n" << graph);
+    CHECK(result.size() == expectedSize);
+}
+
+void reportSet(RelationsGraph &graph, Bucket &one, RelationType rel,
+               Bucket &two) {
+    INFO("setting " << one.id << " " << rel << " " << two.id);
+    graph.addRelation(one, rel, two);
+    INFO("done");
+}
+
+void checkEdges(const RelationsGraph &graph, size_t relationsSet) {
+    SECTION("all") {
+        CollectedEdges result = collect(graph.begin(allRelations), graph.end());
+        checkSize(result, graph, relationsSet * 2);
+    }
+    SECTION("undirected") {
+        CollectedEdges result = collect(graph.begin(), graph.end());
+        checkSize(result, graph, relationsSet);
+    }
+}
+
+RelationType generateNonEQRelation() {
+    return toRelation(GENERATE(range(1ul, allRelations.size())));
+}
+
+RelationType generateRelation() {
+    return toRelation(GENERATE(range(0ul, allRelations.size())));
+}
+
 TEST_CASE("edge iterator") {
     RelationsGraph graph;
 
@@ -38,18 +71,40 @@ TEST_CASE("edge iterator") {
     SECTION("two nodes") {
         REQUIRE(graph.begin() == graph.end());
 
-        size_t relIndex = GENERATE(range(0ul, allRelations.size()));
-        DYNAMIC_SECTION("setting " << toRelation(relIndex)) {
-            graph.addRelation(one, toRelation(relIndex), two);
+        RelationType relation = generateNonEQRelation();
 
-            CollectedEdges result =
-                    collect(graph.begin(allRelations), graph.end());
-            if (toRelation(relIndex) == RelationType::EQ) {
-                CHECK(result.size() == 0);
-            } else {
-                INFO(dump(result));
-                INFO(graph);
-                CHECK(result.size() == 2);
+        DYNAMIC_SECTION("setting " << relation) {
+            reportSet(graph, one, relation, two);
+
+            checkEdges(graph, 1);
+        }
+    }
+
+    Bucket &three = graph.getNewBucket();
+
+    SECTION("three nodes") {
+        REQUIRE(graph.begin() == graph.end());
+
+        RelationType relOne = generateNonEQRelation();
+        RelationType relTwo = generateNonEQRelation();
+
+        DYNAMIC_SECTION("setting " << relOne << " " << relTwo) {
+            SECTION("chain") {
+                reportSet(graph, one, relOne, two);
+                reportSet(graph, two, relTwo, three);
+                checkEdges(graph, 2);
+            }
+
+            SECTION("fork 2 - 1 - 3") {
+                reportSet(graph, one, relOne, two);
+                reportSet(graph, one, relTwo, three);
+                checkEdges(graph, 2);
+            }
+
+            SECTION("other fork 3 - 2 - 1") {
+                reportSet(graph, three, relOne, two);
+                reportSet(graph, one, relTwo, two);
+                checkEdges(graph, 2);
             }
         }
     }
@@ -62,10 +117,10 @@ TEST_CASE("testing relations") {
     Bucket &two = graph.getNewBucket();
 
     SECTION("unrelated") {
-        size_t relIndex = GENERATE(range(0ul, allRelations.size()));
-        DYNAMIC_SECTION(toRelation(relIndex)) {
-            REQUIRE(!graph.areRelated(one, toRelation(relIndex), two));
-            REQUIRE(!graph.areRelated(two, toRelation(relIndex), one));
+        RelationType rel = generateRelation();
+        DYNAMIC_SECTION(rel) {
+            REQUIRE(!graph.areRelated(one, rel, two));
+            REQUIRE(!graph.areRelated(two, rel, one));
         }
     }
 
@@ -74,16 +129,15 @@ TEST_CASE("testing relations") {
         REQUIRE(graph.areRelated(two, RelationType::EQ, two));
     }
 
-    size_t relIndex = GENERATE(range(0ul, allRelations.size()));
-    DYNAMIC_SECTION("set and test " << toRelation(relIndex)) {
-        graph.addRelation(one, toRelation(relIndex), two);
-        INFO(one);
-        if (toRelation(relIndex) == RelationType::EQ)
-            CHECK(graph.areRelated(one, toRelation(relIndex), one));
+    RelationType rel = generateRelation();
+    DYNAMIC_SECTION("set and test " << rel) {
+        graph.addRelation(one, rel, two);
+        INFO(graph);
+        if (rel == RelationType::EQ)
+            CHECK(graph.areRelated(one, rel, one));
         else {
-            INFO(two);
-            std::cerr << toRelation(relIndex) << std::endl;
-            CHECK(graph.areRelated(one, toRelation(relIndex), two));
+            // INFO(two);
+            CHECK(graph.areRelated(one, rel, two));
         }
     }
 
